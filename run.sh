df05da8d3b975068c90e50728c5e411ae2f3e978
domschrei <mail@dominikschreiber.de>
2019-10-07 09:15:32 +0200

    Update runscript

--- run.sh
+++ run.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-if [ "$1" ]; then 
-    NP="$1"
-else
-    NP=9
-    echo "Launching with 9 nodes."
+cmd=""
+if [ x"$1" == x"valgrind" ]; then
+    cmd="valgrind"
+    shift 1
 fi
 
-mpirun -np "$NP" ./mallob | tee logs/log.`date +%s`
+# Number of nodes to launch
+if echo "$1" | grep -qE "^[0-9]+$" ; then 
+    NP="$1"
+    shift 1
+else
+    NP="9"
+    echo "Defaulting to $NP nodes."
+fi
+
+# Resource scheduling / get tokens for mpi processes
+if command -v nonexclusive ; then
+    cmd="$cmd nonexclusive"
+fi
+
+# Logging directory
+mkdir -p logs
+
+# Execute program
+echo 'mpirun -np "'$NP'" '$cmd' ./mallob '$@' | tee logs/log.'`date +%s`
+mpirun -np "$NP" $cmd ./mallob $@ | tee logs/log.`date +%s`
