04401f61f2ca0969928f3a46f8e2dd789eec224f
domschrei <mail@dominikschreiber.de>
2019-11-05 10:46:39 +0100

    New scenario with very many simple jobs

--- create_random_scenario.sh
+++ create_random_scenario.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function rand_arrival() {
-    awk -v n=1 -v seed="$RANDOM" 'BEGIN { srand(seed); for (i=0; i<n; ++i) printf("%.4f\n", 10*rand()+'$1') }'
+    awk -v n=1 -v seed="$RANDOM" 'BEGIN { srand(seed); for (i=0; i<n; ++i) printf("%.4f\n", '$2'*rand()+'$1') }'
 }
 function rand_priority() {
     awk -v n=1 -v seed="$RANDOM" 'BEGIN { srand(seed); for (i=0; i<n; ++i) printf("%.4f\n", 0.998*rand()+0.001) }'
@@ -16,10 +16,17 @@ arrival=0
 echo "# ID Arv Prio File"
 while read -r filename; do
 
-    arrival=`rand_arrival $arrival`
+    arrival=`rand_arrival $arrival 10`
     echo "$id $arrival `rand_priority` $filename"
     id=$((id+1))
 
 done < selection
 
 rm selection
+
+arrival=0
+for i in {1..1000}; do
+    arrival=`rand_arrival $arrival 2`
+    echo $(($id+100000))" $arrival "`rand_priority`" /global_data/schreiber/sat_instances/ridiculouslysimple_sat_1.cnf"
+    id=$((id+1))
+done
