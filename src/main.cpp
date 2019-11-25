112f9d1a05861c9cd31240d88f0d1e2f32b6fd8d
domschrei <mail@dominikschreiber.de>
2019-11-25 13:30:50 +0100

    Introduce quiet param to suppress logging to stdout

--- src/main.cpp
+++ src/main.cpp
@@ -65,7 +65,8 @@ int main(int argc, char *argv[]) {
 
     Parameters params;
     params.init(argc, argv);
-    Console::init(rank, params.getIntParam("v"), params.isSet("colors"), /*threadsafeOutput=*/false, params.getParam("log"));
+    Console::init(rank, params.getIntParam("v"), params.isSet("colors"), 
+            /*threadsafeOutput=*/false, /*quiet=*/params.isSet("q"), params.getParam("log"));
     
     if (rank == 0)
         params.printParams();
