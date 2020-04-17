c53c375511b5fa07a94af6bcab40a033b9e8ab98
Dominik Schreiber <mail@dominikschreiber.de>
2020-04-17 10:56:48 +0200

    Debug reworking logger of Hordesat

--- src/hordesat/incremental-hordesat/HordeLib.h
+++ src/hordesat/incremental-hordesat/HordeLib.h
@@ -10,7 +10,6 @@
 
 #include "utilities/ParameterProcessor.h"
 #include "utilities/Threading.h"
-#include "utilities/verbose_mutex.h"
 #include "utilities/logging_interface.h"
 #include "solvers/MiniSat.h"
 #include "solvers/Lingeling.h"
