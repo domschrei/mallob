131102cf5905158a4da992d18ff258f942ce04e4
Dominik Schreiber <mail@dominikschreiber.de>
2020-04-20 17:25:44 +0200

    Debug

--- src/worker.cpp
+++ src/worker.cpp
@@ -1152,7 +1152,7 @@ void Worker::allreduceSystemState() {
                 reducingSystemState = false;
                 int verb = (worldRank == 0 ? Console::INFO : Console::VVVVERB);
                 Console::log(verb, "sysstate busy=%.2f%% jobs=%i accmem=%.2fGB", 100*systemState[0]/MyMpi::size(comm), (int)systemState[1], systemState[2]);
-            } else if (lastSystemStateReduce > 0 && timeSinceLast > 10) {
+            } else if (lastSystemStateReduce > 0 && timeSinceLast > 30) {
                 Console::log(Console::CRIT, "Unresponsive node(s) since 10 seconds! Aborting");
                 abort();
             }
