5479a719a92c68c9327dad84468a616821a37fdc
Dominik Schreiber <mail@dominikschreiber.de>
2020-07-07 08:53:35 +0200

    Close shmem file descriptor after creating / accessing shmem

--- src/util/sys/shared_memory.cpp
+++ src/util/sys/shared_memory.cpp
@@ -19,6 +19,7 @@ namespace SharedMemory {
 
         void *buffer = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, memFd, 0);
         assert(buffer != nullptr);
+        close(memFd);
 
         return buffer;
     }
@@ -36,6 +37,7 @@ namespace SharedMemory {
             perror("Can't mmap");
             return nullptr;
         }
+        close(memFd);
 
         return buffer;
     }
