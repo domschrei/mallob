9eacef9eaf05d2e045041a80525f544929c171f8
Maximilian Schick <maxi.f.schick@gmail.com>
2020-09-22 14:57:06 +0200

    Remove all race conditions in cadical_terminator

--- src/app/sat/hordesat/solvers/cadical_terminator.hpp
+++ src/app/sat/hordesat/solvers/cadical_terminator.hpp
@@ -1,32 +1,33 @@
-
-#include "app/sat/hordesat/utilities/logging_interface.hpp"
-#include "util/sys/threading.hpp"
+#include <atomic>
+#include <cassert>
 
 #include "app/sat/hordesat/solvers/cadical_interface.hpp"
+#include "app/sat/hordesat/utilities/logging_interface.hpp"
+#include "util/sys/threading.hpp"
 
 struct HordeTerminator : public CaDiCaL::Terminator {
     HordeTerminator(LoggingInterface &logger) : _logger(logger) {
         _lastTermCallbackTime = logger.getTime();
+        _runningMutex = _suspendMutex.getLock();
     };
-    ~HordeTerminator() override {}
 
     bool terminate() override {
         double elapsed = _logger.getTime() - _lastTermCallbackTime;
         _lastTermCallbackTime = _logger.getTime();
 
-        if (_stop) {
+        if (_interrupt.load()) {
             _logger.log(1, "STOP (%.2fs since last cb)", elapsed);
             return true;
         }
 
-        if (_suspend) {
-            // Stay inside this function call as long as solver is suspended
+        if (_suspend.load()) {
             _logger.log(1, "SUSPEND (%.2fs since last cb)", elapsed);
 
-            _suspendCond.wait(_suspendMutex, [this] { return !_suspend; });
+            // Stay inside this function call as long as solver is suspended
+            _suspendCond.wait(_runningMutex, [this] { return !_suspend; });
             _logger.log(2, "RESUME");
 
-            if (_stop) {
+            if (_interrupt.load()) {
                 _logger.log(2, "STOP after suspension", elapsed);
                 return true;
             }
@@ -35,26 +36,35 @@ struct HordeTerminator : public CaDiCaL::Terminator {
     }
 
     void setInterrupt() {
-        _stop = 1;
+        _interrupt.store(true);
     }
     void unsetInterrupt() {
-        _stop = 0;
+        _interrupt.store(false);
     }
     void setSuspend() {
-        _suspend = true;
+        _suspend.store(true);
     }
     void unsetSuspend() {
-        _suspend = false;
+        // Assert that the solver was previously suspended.
+        // Alternative: The method simply returns if _suspend is not set to true.
+        assert(_suspend.load() == true);
+        // Wait for _suspendMutex to be aquired. This guarantees that the solver is suspended.
+        // The _suspendMutex is aquired during the initialization of this terminator, therefore it happens before any calls to unsetSuspend.
+        const std::lock_guard<Mutex> lock(_suspendMutex);
+        _suspend.store(false);
         _suspendCond.notify();
     }
 
-private:
+   private:
     LoggingInterface &_logger;
     double _lastTermCallbackTime;
 
-    int _stop = 0;
-    volatile bool _suspend = false;
+    std::atomic_bool _interrupt{false};
+    std::atomic_bool _suspend{false};
 
     Mutex _suspendMutex;
+    // Holds unique_lock of _suspendMutex during solving. The mutex is unlocked during suspension.
+    std::unique_lock<std::mutex> _runningMutex;
+
     ConditionVariable _suspendCond;
 };
