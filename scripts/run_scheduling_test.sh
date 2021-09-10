7a245e2543146b3046cf0aebb1637afe9983df63
Dominik Schreiber <mail@dominikschreiber.de>
2021-09-10 17:13:10 +0200

    Improve documentation of scheduling test script

--- scripts/run_scheduling_test.sh
+++ scripts/run_scheduling_test.sh
@@ -2,22 +2,31 @@
 
 set -e
 
+# Parameters to supply:
+# 1st parameter: number of MPI processes to start
+# 2nd parameter (optional): a text file containing one CNF problem file path per line
+
+# 2nd parameter missing?
 if [ -z $2 ]; then
+    # -- collect all CNF problems in the instances/ directory, write a text file
     cd instances
     > _benchmark_local
     for f in *.cnf ; do
        echo $f >> _benchmark_local
     done
-    shuf _benchmark_local -o _benchmark_local
+    shuf _benchmark_local -o _benchmark_local # shuffle lines randomly
     cd ..
     benchmarkfile="instances/_benchmark_local"
 else
+    # use the user-supplied benchmark file
     benchmarkfile=$2
 fi
 
+# Import important functions (see scripts/systest_commons.sh)
 testcount=1
 source $(dirname "$0")/systest_commons.sh
 
+# Directory setup
 mkdir -p .api/jobs.0/
 mkdir -p .api/jobs.0/{introduced,new,pending,done}/
 cleanup
@@ -25,15 +34,15 @@ cleanup
 # Generate chain of independent jobs
 i=1
 while read -r instance; do
-    # wallclock limit, arrival, dependencies, application
-    introduce_job solve-$i instances/$instance 300 0 "" SAT #DUMMY
+    # Parameters: job name, instance path, wallclock limit, arrival, dependencies, application (SAT or DUMMY)
+    introduce_job solve-$i instances/$instance 300 0 "" SAT
     i=$((i+1))
 done < $benchmarkfile
 
 # Set options
 options="-t=4 -lbc=4 -g=0.1 -satsolver=lcg -v=4 -J=$(($i-1)) -ch=1 -chaf=5 -chstms=60 -cfhl=0 -smcl=30 -hmcl=30 -mlbdps=8 -checksums=0 -huca=0 -wam=1000 -sleep=100"
 
-# Launch Mallob
+# Launch Mallob with a unique run ID for a logging directory
 runid="sched_$(hostname)_$(git rev-parse --short HEAD)_np${1}_"$(echo $options|sed 's/-//g'|sed 's/=//g'|sed 's/ /_/g')
 RDMAV_FORK_SAFE=1 PATH=build/:$PATH nohup mpirun -np $1 --oversubscribe build/mallob -log=runs/$runid $options 2>&1 > OUT &
 
