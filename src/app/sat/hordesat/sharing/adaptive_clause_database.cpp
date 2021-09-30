1d48fdad585fcffe055de93d78ce8c28c6990795
Dominik Schreiber <mail@dominikschreiber.de>
2021-09-30 16:40:35 +0200

    Refactoring and comments

---
+++ src/app/sat/hordesat/sharing/adaptive_clause_database.cpp
@@ -0,0 +1,241 @@
+
+#include "adaptive_clause_database.hpp"
+
+AdaptiveClauseDatabase::AdaptiveClauseDatabase(int maxClauseSize, int maxLbdPartitionedSize, 
+        int baseBufferSize, int numChunks, int numProducers, bool useChecksum):
+    _max_lbd_partitioned_size(maxLbdPartitionedSize), 
+    _chunk_size(baseBufferSize), 
+    _use_checksum(useChecksum),
+    _hist_deleted_in_slots(maxClauseSize) {
+        
+    _chunk_mgr = ChunkManager(numChunks, baseBufferSize);
+    
+    // Initialize a clause slot for each length-LBD bucket
+    BucketLabel l;
+    while (l.size <= maxClauseSize) {
+
+        _slots.push_back(ClauseSlot(l.size, l.lbd, l.size <= maxLbdPartitionedSize, _chunk_size, numProducers));
+        int slotIdx = _slots.size()-1;
+        auto& slot = _slots.back();
+
+        // Connect clause slot with clause manager for returning unused chunks
+        slot.setChunkSink([&](int* data) {
+            _chunk_mgr.insertChunk(data);
+        });
+
+        // Connect clause slot with a means to retrieve "new" chunks
+        slot.setChunkSource([this, slotIdx]() {
+            std::pair<ClauseSlot::SlotResult, int*> pair {ClauseSlot::TOTAL_FAIL, nullptr};
+
+            // First try to get a chunk from the chunk manager
+            int* chunk = _chunk_mgr.getChunkOrNullptr();
+            if (chunk == nullptr) {
+                // No chunks available right now.
+                // Try to steal chunk from a slot to the right 
+                // (which is, consequently, less important than this one).
+                for (size_t i = _slots.size()-1; i > slotIdx; i--) {
+                    auto result = _slots[i].releaseChunk(chunk);
+                    // Try another slot on total fail; stop otherwise
+                    if (result != ClauseSlot::TOTAL_FAIL) {
+                        pair.first = result;
+                        break;
+                    }
+                }
+            } else pair.first = ClauseSlot::SUCCESS;
+
+            pair.second = chunk;
+            return pair;
+        });
+
+        slot.setDeletedClausesHistogram(_hist_deleted_in_slots);
+
+        l.next(maxLbdPartitionedSize);
+    }
+}
+
+
+AdaptiveClauseDatabase::AddClauseResult AdaptiveClauseDatabase::addClause(int producerId, const Clause& c) {
+
+    // Find correct clause slot, attempt to insert
+    size_t slotIdx = getSlotIdx(c.size, c.lbd);
+    if (slotIdx < 0 || slotIdx >= _slots.size()) {
+        log(V1_WARN, "[WARN] %s -> invalid slot index %i\n", c.toStr().c_str(), slotIdx);
+        return DROP; // clause is not acceptable
+    }
+    auto& slot = _slots[slotIdx];
+    auto result = slot.insert(producerId, c);
+    if (result == ClauseSlot::SlotResult::SUCCESS) return SUCCESS;
+    // Fail spuriously if a resource inside was busy
+    if (result == ClauseSlot::SlotResult::SPURIOUS_FAIL) return TRY_LATER;
+    return DROP; // total failure
+}
+
+void AdaptiveClauseDatabase::bulkAddClauses(int producerId, const std::vector<Clause>& clauses, 
+        std::list<Clause>& deferredOut, SolvingStatistics& stats,
+        std::function<bool(const Clause& c)> conditional) {
+
+    // As long as there are clauses left:
+    const Clause* cPtr = clauses.data();
+    while (cPtr != clauses.data()+clauses.size()) {
+        auto& clause = *cPtr;
+        assert(clause.begin != nullptr);
+
+        // Find correct slot to insert
+        size_t slotIdx = getSlotIdx(clause.size, clause.lbd);
+        if (slotIdx < 0 || slotIdx >= _slots.size()) {
+            cPtr++;
+            continue;
+        }
+        // Insert as many clauses as you can, setting cPtr to the first clause *not* added
+        _slots[slotIdx].insert(producerId, cPtr, clauses.data()+clauses.size(), deferredOut, stats, conditional);
+    }
+}
+
+void AdaptiveClauseDatabase::printChunks(int nextExportSize) {
+    size_t i = 0;
+    BucketLabel l;
+    int virtuallyExported = 0;
+    for (auto& slot : _slots) {
+        int effectiveClauseSize = l.size + (l.size > _max_lbd_partitioned_size ? 1 : 0);
+        int capacityPerChunk = _chunk_size / effectiveClauseSize;
+        auto fillStates = slot.getChunkFillStates();
+        std::string out = "";
+        for (size_t i = 0; i < fillStates.size(); i++) {
+            
+            int numElems = fillStates[i];
+            if (numElems == -1) {
+                out += "  ---   ";
+                continue;
+            }
+
+            out += "[";
+            
+            float fillRatio = ((float)numElems) / capacityPerChunk;
+            bool readFrom = (nextExportSize >= 0 && virtuallyExported < nextExportSize);
+            virtuallyExported += numElems * effectiveClauseSize;
+            bool readAll = (nextExportSize >= 0 && virtuallyExported <= nextExportSize);
+            if (readAll) out += "\033[34m";
+            else if (readFrom) out += "\033[36m";
+
+            if (fillRatio <= 0)   out += ".....";
+            else if (fillRatio <= 0.2) out += "|....";
+            else if (fillRatio <= 0.4) out += "||...";
+            else if (fillRatio <= 0.6) out += "|||..";
+            else if (fillRatio <= 0.8) out += "||||.";
+            else                       out += "|||||";
+            if (nextExportSize >= 0) out += "\033[0m";
+            
+            out += "] ";
+        }
+        log(V2_INFO, "CDB (%i,%i) %i \t%s\n", l.size, l.lbd, slot.getNumActiveChunks(), out.c_str());
+        i++;
+        l.next(_max_lbd_partitioned_size);
+    }
+}
+
+std::vector<int> AdaptiveClauseDatabase::exportBuffer(int sizeLimit, int& numExportedClauses, 
+        const int minClauseLength, const int maxClauseLength) {
+
+    int zero = 0;
+    numExportedClauses = 0;
+                
+    std::vector<int> selection;
+    if (sizeLimit > 0) selection.reserve(sizeLimit);
+    size_t hash = 1;
+
+    // Reserve space for checksum at the beginning of the buffer
+    int numInts = (sizeof(size_t)/sizeof(int));
+    for (int i = 0; i < numInts; i++) selection.push_back(0);
+
+    BucketLabel bucket;
+    int bufIdx = 0;
+    while ((sizeLimit < 0 || selection.size() < sizeLimit) 
+            && bufIdx < _slots.size()) {
+        
+        // Get size and LBD of current bucket
+        int clauseSize = bucket.size;
+        int clauseLbd = bucket.lbd;
+        
+        // The counter for the clauses of this bucket goes here
+        selection.push_back(0);
+
+        // Check that this bucket has the desired clause length
+        if ((minClauseLength < 0 || clauseSize >= minClauseLength) 
+            && (maxClauseLength < 0 || clauseSize <= maxClauseLength)) {
+
+            // Counter integer for the clauses of this bucket
+            int counterPos = selection.size()-1;
+            bool partitionedByLbd = clauseSize <= _max_lbd_partitioned_size;
+
+            // Fetch correct buffer list
+            int effectiveClsLen = clauseSize + (partitionedByLbd ? 0 : 1);
+            auto& buf = _slots[bufIdx];
+
+            // Fetch as many clauses as available and as there is space
+            if (sizeLimit < 0 || selection.size()+effectiveClsLen <= sizeLimit) {
+                // Write next clauses
+                size_t sizeBefore = selection.size();
+                int numDesired = sizeLimit < 0 ? -1 : (sizeLimit - selection.size()) / effectiveClsLen;
+                int received = buf.getClauses(selection, numDesired);
+                selection[counterPos] += received;
+                numExportedClauses += received;
+                if (_use_checksum) {
+                    for (size_t pos = sizeBefore; pos < selection.size(); pos += effectiveClsLen) {
+                        hash_combine(hash, ClauseHasher::hash(
+                            selection.data()+pos,
+                            clauseSize+(partitionedByLbd ? 0 : 1), 3
+                        ));
+                    }
+                }
+            }
+        }
+
+        // Proceed to next bucket
+        bucket.next(_max_lbd_partitioned_size);
+        bufIdx++;
+    }
+
+    // Also iterate over all subsequent clause slots to recognize unused / stale chunks
+    while (bufIdx < _slots.size()) {
+        _slots[bufIdx].getClauses(selection, /*maxNumClauses=*/0);
+        bufIdx++;
+    }
+
+    // Remove trailing zeroes
+    size_t lastNonzeroIdx = selection.size()-1;
+    while (lastNonzeroIdx > 0 && selection[lastNonzeroIdx] == 0) lastNonzeroIdx--;
+    selection.resize(lastNonzeroIdx+1);
+
+    // Write final hash checksum
+    memcpy(selection.data(), &hash, sizeof(size_t));
+
+    return selection;
+}
+
+BufferReader AdaptiveClauseDatabase::getBufferReader(int* begin, size_t size, bool useChecksums) {
+    return BufferReader(begin, size, _max_lbd_partitioned_size, useChecksums);
+}
+
+BufferMerger AdaptiveClauseDatabase::getBufferMerger() {
+    return BufferMerger(_max_lbd_partitioned_size, _use_checksum);
+}
+
+ClauseHistogram& AdaptiveClauseDatabase::getDeletedClausesHistogram() {
+    return _hist_deleted_in_slots;
+}
+
+size_t AdaptiveClauseDatabase::getSlotIdx(int clauseSize, int lbd) {
+    
+    const int mlbdps = _max_lbd_partitioned_size;
+    const int numPartitionedLengthsBefore = std::min(clauseSize-1, mlbdps);
+    const int numPartitionedSlotsBefore = numPartitionedLengthsBefore == 0 ? 
+        0 : 1 + numPartitionedLengthsBefore*(numPartitionedLengthsBefore-1) / 2;
+    const int numNonpartitionedSlotsBefore = std::max(0, clauseSize - mlbdps - 1);
+    const bool isLbdPartitioned = clauseSize <= mlbdps;
+    
+    const size_t index = numPartitionedSlotsBefore + numNonpartitionedSlotsBefore 
+        + (isLbdPartitioned ? std::max(lbd, 2)-2 : 0);
+    
+    if (index < _slots.size()) return index;
+    else return -1;
+}
--- src/app/sat/hordesat/sharing/adaptive_clause_database.hpp
+++ src/app/sat/hordesat/sharing/adaptive_clause_database.hpp
@@ -12,10 +12,29 @@
 #include "util/periodic_event.hpp"
 #include "app/sat/hordesat/solvers/solver_statistics.hpp"
 
+/*
+Buffering structure for clauses which sorts and prioritizes clauses 
+by length (primary) and LBD score (secondary). The structure is adaptive
+because memory chunks of fixed size are allocated on demand and
+can be moved freely from one length-LBD slot to another as necessary.
+*/
 class AdaptiveClauseDatabase {
 
+public:
+    enum AddClauseResult {SUCCESS, TRY_LATER, DROP};
+
 private:
+    /*
+    Initially all chunks are part of this manager (virtually and not yet allocated).
+    Queries for new chunks go here, and unneeded chunks are returned here.
+    */
     ChunkManager _chunk_mgr;
+    /*
+    There is one slot for each length-LBD combination up to a certain clause length
+    (_max_lbd_partitioned_size) and then one slot for each clause length.
+    A slot itself consists of a flexible number of memory chunks with one lock-free
+    MPSC ring buffer operating on each chunk.
+    */
     std::vector<ClauseSlot> _slots;
 
     int _max_lbd_partitioned_size;
@@ -25,235 +44,68 @@ private:
     ClauseHistogram _hist_deleted_in_slots;
 
 public: 
-    AdaptiveClauseDatabase(int maxClauseSize, int maxLbdPartitionedSize, int baseBufferSize, int numChunks, int numProducers, bool useChecksum = false):
-            _max_lbd_partitioned_size(maxLbdPartitionedSize), _chunk_size(baseBufferSize), _use_checksum(useChecksum),
-            _hist_deleted_in_slots(maxClauseSize) {
-        
-        _chunk_mgr = ChunkManager(numChunks, baseBufferSize);
-        
-        BucketLabel l;
-        while (l.size <= maxClauseSize) {
-
-            _slots.push_back(ClauseSlot(l.size, l.lbd, l.size <= maxLbdPartitionedSize, _chunk_size, numProducers));
-            int slotIdx = _slots.size()-1;
-
-            _slots.back().setChunkSink([&](int* data) {
-                _chunk_mgr.insertChunk(data);
-            });
-
-            _slots.back().setChunkSource([this, slotIdx]() {
-                std::pair<ClauseSlot::SlotResult, int*> pair {ClauseSlot::TOTAL_FAIL, nullptr};
-
-                int* chunk = _chunk_mgr.getChunkOrNullptr();
-                if (chunk == nullptr) {
-                    // No chunks available right now.
-                    // Try to steal chunk from slot to the right.
-                    for (size_t i = _slots.size()-1; i > slotIdx; i--) {
-                        auto result = _slots[i].releaseChunk(chunk);
-                        // Try prev. slot on total fail; stop otherwise
-                        if (result != ClauseSlot::TOTAL_FAIL) {
-                            pair.first = result;
-                            break;
-                        }
-                    }
-                } else pair.first = ClauseSlot::SUCCESS;
-
-                pair.second = chunk;
-                return pair;
-            });
-
-            _slots.back().setDeletedClausesHistogram(_hist_deleted_in_slots);
-
-            l.next(maxLbdPartitionedSize);
-        }
-    }
-
+    AdaptiveClauseDatabase(int maxClauseSize, int maxLbdPartitionedSize, int baseBufferSize, 
+        int numChunks, int numProducers, bool useChecksum = false);
     ~AdaptiveClauseDatabase() = default;
 
-    enum AddClauseResult {SUCCESS, TRY_LATER, DROP};
-    AddClauseResult addClause(int producerId, const Clause& c) {
+    /*
+    Insert a clause from a certain producer (0 <= ID < #producers).
+    SUCCESS: The clause has been inserted.
+    TRY_LATER: A spurious failure occurred due to the internal lock-free
+    data structures: The clause should be deferred and inserted at a
+    later time.
+    DROP: The clause has been rejected due to lack of space relative 
+    to the clause's importance and can be discarded (although it may 
+    be possible to insert it later).
+    In all cases, c can be freed or reused after calling this method.
+    */
+    AddClauseResult addClause(int producerId, const Clause& c);
 
-        // Find correct clause slot, attempt to insert
-        size_t slotIdx = getSlotIdx(c.size, c.lbd);
-        if (slotIdx < 0 || slotIdx >= _slots.size()) {
-            log(V1_WARN, "[WARN] %s -> invalid slot index %i\n", c.toStr().c_str(), slotIdx);
-            return DROP; // clause is not acceptable
-        }
-        auto& slot = _slots[slotIdx];
-        auto result = slot.insert(producerId, c);
-        if (result == ClauseSlot::SlotResult::SUCCESS) return SUCCESS;
-        // Fail spuriously if a resource inside was busy
-        if (result == ClauseSlot::SlotResult::SPURIOUS_FAIL) return TRY_LATER;
-        return DROP; // total failure
-    }
+    /*
+    Add multiple clauses at once. More efficient than addClause for a series of clauses
+    because the search for a fitting slot and chunk for each clause is not done redundantly.
+    The clauses *must* be sorted with respect to the buckets of this instance, i.e.,
+    best clauses come first, as retrieved by calling exportBuffer and then iterating
+    over the clauses via getBufferReader.
+    As in adding a single clause, spurious failures may occur causing a clause to be
+    deferred - each such clause will be returned to the caller via the supplied clause list.
+    A conditional can be supplied to this function which decides for each clause whether
+    it should be inserted or not. It is ensured that this conditional is only called once
+    for each clause.
+    */
+    void bulkAddClauses(int producerId, const std::vector<Clause>& clauses, std::list<Clause>& deferredOut, 
+            SolvingStatistics& stats, std::function<bool(const Clause& c)> conditional = [](const Clause&) {return true;});
 
-    void bulkAddClauses(int producerId, const std::vector<Clause>& clauses, std::list<Clause>& deferredOut, SolvingStatistics& stats,
-            std::function<bool(const Clause& c)> conditional = [](const Clause&) {return true;}) {
-
-        const Clause* cPtr = clauses.data();
-        while (cPtr != clauses.data()+clauses.size()) {
-            auto& clause = *cPtr;
-            assert(clause.begin != nullptr);
-            size_t slotIdx = getSlotIdx(clause.size, clause.lbd);
-            if (slotIdx < 0 || slotIdx >= _slots.size()) {
-                cPtr++;
-                continue;
-            }
-            // Insert as many clauses as you can, setting cPtr to the first clause *not* added
-            _slots[slotIdx].insert(producerId, cPtr, clauses.data()+clauses.size(), deferredOut, stats, conditional);
-            //log(V2_INFO, "BULKADD (%p,%p) -> %p\n", clauses.data(), clauses.data()+clauses.size(), cPtr);
-        }
-    }
-
-    void printChunks(int nextExportSize = -1) {
-        size_t i = 0;
-        BucketLabel l;
-        int virtuallyExported = 0;
-        for (auto& slot : _slots) {
-            int effectiveClauseSize = l.size + (l.size > _max_lbd_partitioned_size ? 1 : 0);
-            int capacityPerChunk = _chunk_size / effectiveClauseSize;
-            auto fillStates = slot.getChunkFillStates();
-            std::string out = "";
-            for (size_t i = 0; i < fillStates.size(); i++) {
-                
-                int numElems = fillStates[i];
-                if (numElems == -1) {
-                    out += "  ---   ";
-                    continue;
-                }
-
-                out += "[";
-                
-                float fillRatio = ((float)numElems) / capacityPerChunk;
-                bool readFrom = (nextExportSize >= 0 && virtuallyExported < nextExportSize);
-                virtuallyExported += numElems * effectiveClauseSize;
-                bool readAll = (nextExportSize >= 0 && virtuallyExported <= nextExportSize);
-                if (readAll) out += "\033[34m";
-                else if (readFrom) out += "\033[36m";
-
-                if (fillRatio <= 0)   out += ".....";
-                else if (fillRatio <= 0.2) out += "|....";
-                else if (fillRatio <= 0.4) out += "||...";
-                else if (fillRatio <= 0.6) out += "|||..";
-                else if (fillRatio <= 0.8) out += "||||.";
-                else                       out += "|||||";
-                if (nextExportSize >= 0) out += "\033[0m";
-                
-                out += "] ";
-            }
-            log(V2_INFO, "CDB (%i,%i) %i \t%s\n", l.size, l.lbd, slot.getNumActiveChunks(), out.c_str());
-            i++;
-            l.next(_max_lbd_partitioned_size);
-        }
-    }
+    void printChunks(int nextExportSize = -1);
     
+    /*
+    Flushes the clauses of highest priority and writes them into a flat buffer.
+    The buffer first contains a hash value of type size_t and then integers only.
+    For each bucket (size,lbd), the buffer contains a number k followed by
+    size*k integers representing each clause. For each bucket (size), the buffer
+    contains a number k followed by (size+1)*k integers representing each clause,
+    beginning with the LBD value. If minClauseLength and/or maxClauseLength is
+    a positive integer, the buffer will only contain clauses adhering to the 
+    respective limit.
+    The buffer can be parsed via getBufferReader or merged with other buffers via
+    getBufferMerger.
+    */
     std::vector<int> exportBuffer(int sizeLimit, int& numExportedClauses, 
-            const int minClauseLength = -1, const int maxClauseLength = -1) {
+            const int minClauseLength = -1, const int maxClauseLength = -1);
 
-        int zero = 0;
-        numExportedClauses = 0;
-                    
-        std::vector<int> selection;
-        if (sizeLimit > 0) selection.reserve(sizeLimit);
-        size_t hash = 1;
+    /*
+    Allows to iterate over the clauses contained in a flat vector of integers
+    as exported by exportBuffer. Must have been created by an AdaptiveClauseDatabase
+    or a BufferMerger with the same parametrization as this instance.
+    Throughout the life time of the BufferReader, the underlying vector must be valid.
+    */
+    BufferReader getBufferReader(int* begin, size_t size, bool useChecksums = false);
+    BufferMerger getBufferMerger();
 
-        // Reserve space for checksum at the beginning of the buffer
-        int numInts = (sizeof(size_t)/sizeof(int));
-        for (int i = 0; i < numInts; i++) selection.push_back(0);
-
-        BucketLabel bucket;
-        int bufIdx = 0;
-        while ((sizeLimit < 0 || selection.size() < sizeLimit) 
-                && bufIdx < _slots.size()) {
-            
-            // Get size and LBD of current bucket
-            int clauseSize = bucket.size;
-            int clauseLbd = bucket.lbd;
-            
-            // The counter for the clauses of this bucket goes here
-            selection.push_back(0);
-
-            // Check that this bucket has the desired clause length
-            if ((minClauseLength < 0 || clauseSize >= minClauseLength) 
-                && (maxClauseLength < 0 || clauseSize <= maxClauseLength)) {
-
-                // Counter integer for the clauses of this bucket
-                int counterPos = selection.size()-1;
-                bool partitionedByLbd = clauseSize <= _max_lbd_partitioned_size;
-
-                // Fetch correct buffer list
-                int effectiveClsLen = clauseSize + (partitionedByLbd ? 0 : 1);
-                auto& buf = _slots[bufIdx];
-
-                // Fetch as many clauses as available and as there is space
-                if (sizeLimit < 0 || selection.size()+effectiveClsLen <= sizeLimit) {
-                    // Write next clauses
-                    size_t sizeBefore = selection.size();
-                    int numDesired = sizeLimit < 0 ? -1 : (sizeLimit - selection.size()) / effectiveClsLen;
-                    int received = buf.getClauses(selection, numDesired);
-                    selection[counterPos] += received;
-                    numExportedClauses += received;
-                    if (_use_checksum) {
-                        for (size_t pos = sizeBefore; pos < selection.size(); pos += effectiveClsLen) {
-                            hash_combine(hash, ClauseHasher::hash(
-                                selection.data()+pos,
-                                clauseSize+(partitionedByLbd ? 0 : 1), 3
-                            ));
-                        }
-                    }
-                }
-            }
-
-            // Proceed to next bucket
-            bucket.next(_max_lbd_partitioned_size);
-            bufIdx++;
-        }
-
-        // Also iterate over all subsequent clause slots to recognize unused / stale chunks
-        while (bufIdx < _slots.size()) {
-            _slots[bufIdx].getClauses(selection, /*maxNumClauses=*/0);
-            bufIdx++;
-        }
-
-        // Remove trailing zeroes
-        size_t lastNonzeroIdx = selection.size()-1;
-        while (lastNonzeroIdx > 0 && selection[lastNonzeroIdx] == 0) lastNonzeroIdx--;
-        selection.resize(lastNonzeroIdx+1);
-
-        // Write final hash checksum
-        memcpy(selection.data(), &hash, sizeof(size_t));
-
-        return selection;
-    }
-
-    BufferReader getBufferReader(int* begin, size_t size, bool useChecksums = false) {
-        return BufferReader(begin, size, _max_lbd_partitioned_size, useChecksums);
-    }
-
-    BufferMerger getBufferMerger() {
-        return BufferMerger(_max_lbd_partitioned_size, _use_checksum);
-    }
-
-    ClauseHistogram& getDeletedClausesHistogram() {
-        return _hist_deleted_in_slots;
-    }
+    ClauseHistogram& getDeletedClausesHistogram();
 
 private:
-    size_t getSlotIdx(int clauseSize, int lbd) {
-        
-        const int mlbdps = _max_lbd_partitioned_size;
-        const int numPartitionedLengthsBefore = std::min(clauseSize-1, mlbdps);
-        const int numPartitionedSlotsBefore = numPartitionedLengthsBefore == 0 ? 
-            0 : 1 + numPartitionedLengthsBefore*(numPartitionedLengthsBefore-1) / 2;
-        const int numNonpartitionedSlotsBefore = std::max(0, clauseSize - mlbdps - 1);
-        const bool isLbdPartitioned = clauseSize <= mlbdps;
-        
-        const size_t index = numPartitionedSlotsBefore + numNonpartitionedSlotsBefore 
-            + (isLbdPartitioned ? std::max(lbd, 2)-2 : 0);
-        
-        if (index < _slots.size()) return index;
-        else return -1;
-    }
+    size_t getSlotIdx(int clauseSize, int lbd);
 
 };
 
