433d12e2b993c14159f8604086a4426f0fffdb07
Dominik Schreiber <mail@dominikschreiber.de>
2022-01-14 15:57:09 +0100

    Update history extraction script to support commitments too

--- scripts/eval/extract_history_from_logdir.sh
+++ scripts/eval/extract_history_from_logdir.sh
@@ -15,13 +15,17 @@ fi
 bash scripts/eval/merge_rank_log_files.sh $logdir
 
 # Extract "LOAD" and "warmup msg" events from the directory
-cat $logdir/*/*.log|grep -E "LOAD|warmup msg" > runlog
+cat $logdir/*/*.log|grep -E "LOAD|warmup msg|COMMIT" > runlog
 
 # Harmonize timestamps using warmup messages, write into runlog.harmonized
 python3 scripts/eval/harmonize_timestamps.py runlog
 
 # Format and stable sort harmonized load events
 export LC_ALL=C
-cat runlog.harmonized |grep LOAD|sed 's/[#)(+:-]/ /g'|sed 's/  \+/ /g'|sort -g -s|awk '{print $1,$2,$4,$5,$6}' > "$outfile"
+
+# 310.485 44 UNCOMMIT #239:63
+cat runlog.harmonized|grep -E "COMMIT|LOAD"|sed 's/[#)(+:-]/ /g'|sed 's/  \+/ /g'\
+|awk '/UNCOMMIT/ {print $1,$2,"0",$4,$5} / COMMIT/ {print $1,$2,"1",$4,$NF} /LOAD/ {print $1,$2,$4,$5,$6}'\
+|sort -s -g > "$outfile"
 
 echo Wrote to "$outfile".
