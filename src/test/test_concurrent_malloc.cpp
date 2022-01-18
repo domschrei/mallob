43916cfca6364c8ea8b5119dfe2c0a76b5a53d33
Dominik Schreiber <mail@dominikschreiber.de>
2022-01-18 07:56:50 +0100

    Improve malloc test

--- src/test/test_concurrent_malloc.cpp
+++ src/test/test_concurrent_malloc.cpp
@@ -13,30 +13,30 @@
 
 void testConcurrentAllocation() {
 
-    const int nThreads = 128;
-    const int nAllocs = 100;
+    const int nThreads = 4;
+    const int nAllocs = 10000;
 
     std::vector<std::thread> threads;
-    std::vector<std::list<void*>> allocatedMemory;
 
     float time = Timer::elapsedSeconds();
 
-    allocatedMemory.resize(nThreads);
     for (int i = 0; i < nThreads; i++) {
-        threads.emplace_back([nThreads, i, &allocatedMemory]() {
-            usleep(10 * (nThreads-i));
-            log(V2_INFO, "Thread #%i: alloc\n", i);
+        threads.emplace_back([nThreads, i]() {
+            log(V2_INFO, "Thread #%i: alloc+free\n", i);
             // allocate
+            std::list<void*> memory;
             for (int j = 0; j < nAllocs; j++) {
-                size_t memsize = 100'000 + Random::rand() * 99'900'000;
-                allocatedMemory[i].push_back(malloc(memsize));
-                usleep(1000 * 100 * Random::rand());
+                size_t memsize = 1'000'000+1'000*j;
+                void* mem = malloc(memsize);
+                memory.push_back(mem);
+                if (memory.size() >= 100) {
+                    free(memory.front());
+                    memory.pop_front();
+                }
             }
-            log(V2_INFO, "Thread #%i: free\n", i);
-            // free
-            for (void* mem : allocatedMemory[i]) {
-                free(mem);
-                usleep(1000 * 100 * Random::rand());
+            while (!memory.empty()) {
+                free(memory.front());
+                memory.pop_front();
             }
             log(V2_INFO, "Thread #%i: done\n", i);
         });
