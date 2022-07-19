5ab2a5d4fabd7c456762dd67f0c506b4f7089470
Michael Doerr <michael@midoerr.de>
2022-07-19 22:56:55 +0200

    result pdfs

--- Testing/plotClick.cmd
+++ Testing/plotClick.cmd
@@ -1,8 +1,8 @@
 
-set folder=latestResults
+set folder=1_160
 set pcName=i10pc135
 for %%t in (times relSpeedup efficiency) do (
-    for %%k in (5 10 20 30 40 50) do ( 
+    for %%k in (20) do ( Rem for /l %%k in (1, 1, 100) do (
         python ../scripts/plot/plot_curves.py -xy .\%folder%\%%t-%pcName%-%%k.txt -l="k=%%k" -o=.\%folder%\Graph-%%t-%pcName%-%%k.pdf
     )
 )
--- Testing/test.bash
+++ Testing/test.bash
@@ -1,46 +1,46 @@
 #!/bin/bash
 pcName="i10pc135"
-folder="latestResults"
+folder="1_160" #latestResults
 instanceName="benign_traffic" #   mnist784            covtypeShuffle
 instanceFirstLine="115 115 52150" #7 7 209          54 55 581012
-kList=20 # 60 70 80
+kList=(20) # 60 70 80
 # npList=(128 127 64 63 32 31 16 15 8 7 4 3 2 1) 
 countPasses=3
-k=20
-#for k in ${kList[@]}; do
+#k=20
+for k in ${kList[@]}; do
 
 
-    echo "$k $instanceFirstLine" > ./instances/${instanceName}${k}.csv
-    cat ./instances/${instanceName}K.csv >> ./instances/${instanceName}${k}.csv
-#    > ./Testing/${folder}/times-${pcName}-${k}.txt
-    for ((n = 158 ; n < 181 ; ++n)); do #${npList[@]};
-        # v=3 
-        #PATH=build/:$PATH RDMAV_FORKSAVE=1 mpirun -np ${n} -oversubscribe build/mallob -mono-application=KMEANS -mono=./instances/${instanceName}${k}.csv -v=3
-        # v=0 >> ./Testing/times-${k}.txt 
-        meanTime=0
-        for ((i = 0 ; i < $countPasses ; ++i)); do
-            > ./Testing/${folder}/out.txt
-            PATH=build/:$PATH RDMAV_FORKSAVE=1 mpirun -np ${n} -oversubscribe build/mallob -mono-application=KMEANS -mono=./instances/${instanceName}${k}.csv -v=0 > ./Testing/${folder}/out.txt
-            meanTime=$(echo "scale=3; $meanTime + ($(cat ./Testing/${folder}/out.txt |grep "Got Result"|awk '{print $2}'))" | bc)
-            #echo "time $(cat ./Testing/${folder}/out.txt |grep "Got Result"|awk '{print $2}')"
-        done
-        meanTime=$(echo "scale=3; $meanTime / $countPasses" | bc)
-        #echo "$n $meanTime"
-        echo "$n $meanTime" >> ./Testing/${folder}/times-${pcName}-${k}.txt
-        #cat ./Testing/${folder}/out.txt |grep "Got Result"
-    done
-    rm ./instances/${instanceName}${k}.csv
+    #echo "$k $instanceFirstLine" > ./instances/${instanceName}${k}.csv
+    #cat ./instances/${instanceName}K.csv >> ./instances/${instanceName}${k}.csv
+#   # > ./Testing/${folder}/times-${pcName}-${k}.txt
+    #for ((n = 158 ; n < 161 ; ++n)); do #${npList[@]};
+    #    # v=3 
+    #    #PATH=build/:$PATH RDMAV_FORKSAVE=1 mpirun -np ${n} -oversubscribe build/mallob -mono-application=KMEANS -mono=./instances/${instanceName}${k}.csv -v=3
+    #    # v=0 >> ./Testing/times-${k}.txt 
+    #    meanTime=0
+    #    for ((i = 0 ; i < $countPasses ; ++i)); do
+    #        > ./Testing/${folder}/out.txt
+    #        PATH=build/:$PATH RDMAV_FORKSAVE=1 mpirun -np ${n} -oversubscribe build/mallob -mono-application=KMEANS -mono=./instances/${instanceName}${k}.csv -v=0 > ./Testing/${folder}/out.txt
+    #        meanTime=$(echo "scale=3; $meanTime + ($(cat ./Testing/${folder}/out.txt |grep "Got Result"|awk '{print $2}'))" | bc)
+    #        #echo "time $(cat ./Testing/${folder}/out.txt |grep "Got Result"|awk '{print $2}')"
+    #    done
+    #    meanTime=$(echo "scale=3; $meanTime / $countPasses" | bc)
+    #    #echo "$n $meanTime"
+    #    echo "$n $meanTime" >> ./Testing/${folder}/times-${pcName}-${k}.txt
+    #    #cat ./Testing/${folder}/out.txt |grep "Got Result"
+    #done
+    #rm ./instances/${instanceName}${k}.csv
 
 
     t1=$(cat ./Testing/${folder}/times-${pcName}-${k}.txt | grep "^1 " | awk '{print $2}')
     > ./Testing/${folder}/relSpeedup-${pcName}-${k}.txt
     > ./Testing/${folder}/efficiency-${pcName}-${k}.txt
-    for ((n = 1; n < 181; ++n)); do
+    for ((n = 1; n < 161; ++n)); do
         #echo $test
         sRel=$(echo "scale=3; $t1 / $(cat ./Testing/${folder}/times-${pcName}-${k}.txt | grep "^${n} " | awk '{print $2}')" | bc)
         efficiency=$(echo "scale=3; $sRel/$n" | bc)
         echo "$n $sRel" >> ./Testing/${folder}/relSpeedup-${pcName}-${k}.txt
         echo "$n $efficiency" >> ./Testing/${folder}/efficiency-${pcName}-${k}.txt
     done
-#done
+done
 
