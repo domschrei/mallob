0a2443f749fc9c8aeefe8528588bb90b4817bc13
Michael Doerr <michael@midoerr.de>
2022-07-25 14:09:47 +0200

    test 30

--- Testing/plotClick.cmd
+++ Testing/plotClick.cmd
@@ -1,8 +1,8 @@
 
-set folder=1_160
+set folder=strange
 set pcName=i10pc135
 for %%t in (times relSpeedup efficiency) do (
-    for %%k in (20) do ( Rem for /l %%k in (1, 1, 100) do (
+    for %%k in (5 10 20 30 40) do ( Rem for /l %%k in (1, 1, 100) do (
         python ../scripts/plot/plot_curves.py -xy .\%folder%\%%t-%pcName%-%%k.txt -l="k=%%k" -o=.\%folder%\Graph-%%t-%pcName%-%%k.pdf
     )
 )
