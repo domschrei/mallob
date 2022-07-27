6bc214b3ee7104bfb566e028dc7ee5f8f9df9732
Michael Doerr <michael@midoerr.de>
2022-07-27 15:59:13 +0200

    graphs

--- Testing/plotClick.cmd
+++ Testing/plotClick.cmd
@@ -3,12 +3,12 @@ set folder=dTest256core
 set pcName=i10pc138
 for %%t in (times relSpeedup efficiency) do (
     set cm=python ../scripts/plot/plot_curves.py -xy
-    for %%k in (30) do ( Rem for /l %%k in (1, 1, 100) do (
-        for %%d in (500000 300000 100000 50000 10000) do (
-            set cm=!cm! .\%folder%\%%t-%pcName%-%%k-%%d.txt -l="k=%%k d=%%d"
+    for %%k in (10 30 50) do ( Rem for /l %%k in (1, 1, 100) do (
+        for %%n in (100000 300000 500000) do ( Rem  50000 10000
+            set cm=!cm! .\%folder%\%%t-%pcName%-%%k-%%n.txt -l="k=%%k n=%%n"
         )
     )
-    set cm=!cm!  -o=.\%folder%\Graph-%%t-%pcName%-kd.pdf
+    set cm=!cm!  -o=.\%folder%\Graph-%%t-%pcName%-kn.pdf
     call !cm!
 )
 pause
--- Testing/test.sh
+++ Testing/test.sh
@@ -4,47 +4,47 @@ folder="dTest256core" #latestResults
 instanceName="covtypeShuffle" #   mnist784      benign_traffic      covtypeShuffle
 instanceFirstLine="54 55" #7 7 209    115 115 52150      54 55 581012
 kList=(10 50) # 60 70 80
-npList=(160 155 150 145 140 135 130 128 127 125 120 115 110 105 100 95 90 85 80 75 70 65 64 63 60 55 50 45 40 35 32 31 30 25 20 16 15 10 8 7 5 4 3 2 1) #   65 64 63 60 55 50 45 40 35 32 31 30 25 20 16 15 10 8 7 5 4 3 2 1
-dList=(100000 300000 500000)
+wList=(160 155 150 145 140 135 130 128 127 125 120 115 110 105 100 95 90 85 80 75 70 65 64 63 60 55 50 45 40 35 32 31 30 25 20 16 15 10 8 7 5 4 3 2 1) #   65 64 63 60 55 50 45 40 35 32 31 30 25 20 16 15 10 8 7 5 4 3 2 1
+nList=(100000 300000 500000)
 countPasses=2
 
 for k in ${kList[@]}; do
-    for d in ${dList[@]}; do
+    for n in ${nList[@]}; do
 
 
-        echo "$k $instanceFirstLine $d" > ./instances/${instanceName}${k}.csv
-        cat ./instances/${instanceName}K.csv >> ./instances/${instanceName}${k}.csv
-        #> ./Testing/${folder}/times-${pcName}-${k}-${d}.txt
-        > ./Testing/${folder}/out.txt
-        for n in ${npList[@]}; do #((n = 158 ; n < 161 ; ++n));
-            # v=3 
-            #PATH=build/:$PATH RDMAV_FORKSAVE=1 mpirun -np ${n} -oversubscribe build/mallob -mono-application=KMEANS -mono=./instances/${instanceName}${k}.csv -v=3
-            # v=0 >> ./Testing/times-${k}.txt 
-            meanTime=0
-            for ((i = 0 ; i < $countPasses ; ++i)); do
-                > ./Testing/${folder}/out.txt
-                PATH=build/:$PATH RDMAV_FORKSAVE=1 mpirun -np ${n} -oversubscribe build/mallob -mono-application=KMEANS -mono=./instances/${instanceName}${k}.csv -v=3 2>&1 > ./Testing/${folder}/out.txt
-                meanTime=$(echo "scale=3; $meanTime + ($(cat ./Testing/${folder}/out.txt |grep "Got Result"|awk '{print $2}'))" | bc)
-                #echo "time $(cat ./Testing/${folder}/out.txt |grep "Got Result"|awk '{print $2}')"
-            done
-            meanTime=$(echo "scale=3; $meanTime / $countPasses" | bc)
-            #echo "$n $meanTime"
-            echo "$n $meanTime" >> ./Testing/${folder}/times-${pcName}-${k}-${d}.txt
-            #cat ./Testing/${folder}/out.txt |grep "Got Result"
-        done
-        rm ./instances/${instanceName}${k}.csv
+      #  echo "$k $instanceFirstLine $n" > ./instances/${instanceName}${k}.csv
+      #  cat ./instances/${instanceName}K.csv >> ./instances/${instanceName}${k}.csv
+      #  #> ./Testing/${folder}/times-${pcName}-${k}-${n}.txt
+      #  > ./Testing/${folder}/out.txt
+      #  for w in ${wList[@]}; do #((w = 158 ; w < 161 ; ++w));
+      #      # v=3 
+      #      #PATH=build/:$PATH RDMAV_FORKSAVE=1 mpirun -np ${w} -oversubscribe build/mallob -mono-application=KMEANS -mono=./instances/${instanceName}${k}.csv -v=3
+      #      # v=0 >> ./Testing/times-${k}.txt 
+      #      meanTime=0
+      #      for ((i = 0 ; i < $countPasses ; ++i)); do
+      #          > ./Testing/${folder}/out.txt
+      #          PATH=build/:$PATH RDMAV_FORKSAVE=1 mpirun -np ${w} -oversubscribe build/mallob -mono-application=KMEANS -mono=./instances/${instanceName}${k}.csv -v=3 2>&1 > ./Testing/${folder}/out.txt
+      #          meanTime=$(echo "scale=3; $meanTime + ($(cat ./Testing/${folder}/out.txt |grep "Got Result"|awk '{print $2}'))" | bc)
+      #          #echo "time $(cat ./Testing/${folder}/out.txt |grep "Got Result"|awk '{print $2}')"
+      #      done
+      #      meanTime=$(echo "scale=3; $meanTime / $countPasses" | bc)
+      #      #echo "$w $meanTime"
+      #      echo "$w $meanTime" >> ./Testing/${folder}/times-${pcName}-${k}-${n}.txt
+      #      #cat ./Testing/${folder}/out.txt |grep "Got Result"
+      #  done
+      #  rm ./instances/${instanceName}${k}.csv
 
 
-        t1=$(cat ./Testing/${folder}/times-${pcName}-${k}-${d}.txt | grep "^1 " | awk '{print $2}')
-        > ./Testing/${folder}/relSpeedup-${pcName}-${k}-${d}.txt
-        > ./Testing/${folder}/efficiency-${pcName}-${k}-${d}.txt
+        t1=$(cat ./Testing/${folder}/times-${pcName}-${k}-${n}.txt | grep "^1 " | awk '{print $2}')
+        > ./Testing/${folder}/relSpeedup-${pcName}-${k}-${n}.txt
+        > ./Testing/${folder}/efficiency-${pcName}-${k}-${n}.txt
         while read p; do
-            n=$(echo "$p" | awk '{print $1}')
+            w=$(echo "$p" | awk '{print $1}')
             #echo $test
-            sRel=$(echo "scale=3; $t1 / $(cat ./Testing/${folder}/times-${pcName}-${k}-${d}.txt | grep "^${n} " | awk '{print $2}')" | bc)
-            efficiency=$(echo "scale=3; $sRel/$n" | bc)
-            echo "$n $sRel" >> ./Testing/${folder}/relSpeedup-${pcName}-${k}-${d}.txt
-            echo "$n $efficiency" >> ./Testing/${folder}/efficiency-${pcName}-${k}-${d}.txt
-        done <./Testing/${folder}/times-${pcName}-${k}-${d}.txt
+            sRel=$(echo "scale=3; $t1 / $(cat ./Testing/${folder}/times-${pcName}-${k}-${n}.txt | grep "^${w} " | awk '{print $2}')" | bc)
+            efficiency=$(echo "scale=3; $sRel/$w" | bc)
+            echo "$w $sRel" >> ./Testing/${folder}/relSpeedup-${pcName}-${k}-${n}.txt
+            echo "$w $efficiency" >> ./Testing/${folder}/efficiency-${pcName}-${k}-${n}.txt
+        done <./Testing/${folder}/times-${pcName}-${k}-${n}.txt
     done
 done
