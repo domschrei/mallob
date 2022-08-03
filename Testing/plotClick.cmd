507c93b32812a1d89d47faf37ef421091b13d895
Michael Doerr <michael@midoerr.de>
2022-08-03 08:59:01 +0200

    delete empty lines

--- Testing/plotClick.cmd
+++ Testing/plotClick.cmd
@@ -1,10 +1,10 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
-set folder=dTest256core
+set folder=nTest256core100d
 set pcName=i10pc138
 for %%t in (times relSpeedup efficiency) do (
     set cm=python ../scripts/plot/plot_curves.py -xy
     for %%k in (10 30 50) do ( Rem for /l %%k in (1, 1, 100) do (
-        for %%n in (100000 300000 500000) do ( Rem  50000 10000
+        for %%n in (10000 30000 50000) do ( Rem  50000 10000
             set cm=!cm! .\%folder%\%%t-%pcName%-%%k-%%n.txt -l="k=%%k n=%%n"
         )
     )
