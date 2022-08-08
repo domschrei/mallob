af52c30dc09e8467923642a8fdeadd700b592ec4
Michael Doerr <michael@midoerr.de>
2022-08-08 21:25:24 +0200

    Funktion Zusammenfassung

--- Testing/plotClick.cmd
+++ Testing/plotClick.cmd
@@ -3,7 +3,7 @@ set folder=nTest256core50dNochmal
 set pcName=i10pc138
 for %%t in (times relSpeedup efficiency) do (
     set cm=python ../scripts/plot/plot_curves.py -xy -xlabel="num Workers" -ylabel="%%t"
-    for %%k in (10 30 50 70 100) do ( Rem for /l %%k in (1, 1, 100) do (
+    for %%k in (100) do ( Rem for /l %%k in (1, 1, 100) do ( Rem 10 30 50 70 100
         for %%n in (100000 300000 500000) do ( Rem  50000 10000
             set cm=!cm! .\%folder%\%%t-%pcName%-%%k-%%n.txt -l="k=%%k n=%%n"
         )
