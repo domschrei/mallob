e294fe4db76557187a78b2d06b692b2538bb3af1
Michael Doerr <michael@midoerr.de>
2022-08-18 12:28:23 +0200

    prepare old code test

--- Testing/plotClick.cmd
+++ Testing/plotClick.cmd
@@ -1,9 +1,9 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
-set folder=nTest256core50dNochmal
+set folder=nTest256core50dSchneller
 set pcName=i10pc138
 for %%t in (times relSpeedup efficiency) do (
     set cm=python ../scripts/plot/plot_curves.py -xy -xlabel="num Workers" -ylabel="%%t"
-    for %%k in (100) do ( Rem for /l %%k in (1, 1, 100) do ( Rem 10 30 50 70 100
+    for %%k in (10 30 50) do ( Rem for /l %%k in (1, 1, 100) do ( Rem 10 30 50 70 100
         for %%n in (100000 300000 500000) do ( Rem  50000 10000
             set cm=!cm! .\%folder%\%%t-%pcName%-%%k-%%n.txt -l="k=%%k n=%%n"
         )
