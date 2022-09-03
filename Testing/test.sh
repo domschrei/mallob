64faa65df987d720c691cc1370e67b279f14e2c1
Michael Doerr <michael@midoerr.de>
2022-09-03 03:21:56 +0200

    comment and solution verb 2

--- Testing/test.sh
+++ Testing/test.sh
@@ -4,10 +4,10 @@ folder="nTest256core50dFinal" #latestResults
 instanceName="covtypeShuffle" #   mnist784      benign_trafficShuffle      covtypeShuffle
 instanceFirstLine="54 55" #              7 7 209       115 115 52150              54 55 581012
 kList=(10 30 50 100) # 60 70 80 70 100
-wList=(200 180 170 160 150 140 130 128 127 125 120 110 100 90 80 70 65 64 63 60 50 40 35 32 31 30 25 20 16 10 8 7 5 3 2 1) #   65 64 63 60 55 50 45 40 35 32 31 30 25 20 16 15 10 8 7 5 4 3 2 1
+wList=(15) # 200 180 170 160 150 140 130 128 127 125 120 110 100 90 80 70 65 64 63 60 50 40 35 32 31 30 25 20 16 15 10 8 7 5 3 2 1
 nList=(100000 300000 500000) # 
 countPasses=2
-
+echo "$pcName $instanceName $countPasses"> ./Testing/${folder}/info.txt
 ./Testing/killHung.sh ${folder} &
 for k in ${kList[@]}; do
     for n in ${nList[@]}; do
@@ -15,9 +15,8 @@ for k in ${kList[@]}; do
 
         echo "$k $instanceFirstLine $n" > ./instances/${instanceName}${k}.csv
         cat ./instances/${instanceName}K.csv >> ./instances/${instanceName}${k}.csv
-        > ./Testing/${folder}/times-${pcName}-${k}-${n}.txt
+        >> ./Testing/${folder}/times-${pcName}-${k}-${n}.txt
         > ./Testing/${folder}/out.txt
-        echo "$pcName $instanceName $countPasses"> ./Testing/${folder}/info.txt
         for w in ${wList[@]}; do #((w = 158 ; w < 161 ; ++w));
             # v=3 
             #PATH=build/:$PATH RDMAV_FORKSAVE=1 mpirun -np ${w} -oversubscribe build/mallob -mono-application=KMEANS -mono=./instances/${instanceName}${k}.csv -v=3
