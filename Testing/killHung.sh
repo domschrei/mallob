51ef6f3d5c5f3aeaa49b7be847efa646830ef875
Michael Doerr <michael@midoerr.de>
2022-09-03 15:02:00 +0200

    results

--- Testing/killHung.sh
+++ Testing/killHung.sh
@@ -2,8 +2,9 @@
 > ./Testing/${1}/err.txt
 while true; do
     #echo "$(cat ./Testing/${1}/out.txt | grep "ERROR")"
+    echo "$(cat ./Testing/${1}/out.txt | grep "c 10000." | head -1 | awk '{print $4}')"
     sleep 5
-    if [ "$(cat ./Testing/${1}/out.txt | grep "ERROR" | head -1 | awk '{print $4}')" == "[ERROR]" ]; then
+    if [ "$(cat ./Testing/${1}/out.txt | grep "c 10000." | head -1 | awk '{print $4}')" == "sysstate" ] || [ "$(cat ./Testing/${1}/out.txt | grep "ERROR" | head -1 | awk '{print $4}')" == "[ERROR]" ]; then
         echo "found err"
         cat ./Testing/${1}/out.txt >> ./Testing/${1}/err.txt
          echo "_________________________________________________________________" >> ./Testing/${1}/err.txt
