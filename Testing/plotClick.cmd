f98de2f07b443bb03bf32be49219a30ecf1b99fb
Michael Doerr <michael@midoerr.de>
2022-09-05 02:38:15 +0200

    graphs

--- Testing/plotClick.cmd
+++ Testing/plotClick.cmd
@@ -1,10 +1,10 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
-set folder=nTest256core50dSchneller
+set folder=nTest256core50dmax150
 set pcName=i10pc138
 for %%t in (times relSpeedup efficiency) do (
     for %%n in (100000 300000 500000) do ( Rem  50000 10000
         set cm=python ../scripts/plot/plot_curves.py -xy -xlabel="num Workers" -ylabel="%%t"
-        for %%k in (10 30 50 70 100) do ( Rem for /l %%k in (1, 1, 100) do ( Rem 10 30 50 70 100
+        for %%k in (10 30 50 100) do ( Rem for /l %%k in (1, 1, 100) do ( Rem 10 30 50 70 100
             set cm=!cm! .\%folder%\%%t-%pcName%-%%k-%%n.txt -l="k=%%k n=%%n"
         )
         set cm=!cm!  -o=.\%folder%\Graph-%%n-%%t-%pcName%-kn.pdf
--- Testing/test.sh
+++ Testing/test.sh
@@ -7,34 +7,34 @@ kList=(100 50 30 10) # 60 70 80 70 100
 wList=(200 180 170 160 150 140 130 128 127 125 120 110 100 90 80 70 65 64 63 60 50 40 35 32 31 30 25 20 16 15 10 8 7 5 3 2 1) # 200 180 170 160 150 140 130 128 127 125 120 110 100 90 80 70 65 64 63 60 50 40 35 32 31 30 25 20 16 15 10 8 7 5 3 2 1
 nList=(100000 300000 500000) # 
 countPasses=1
-echo "$pcName $instanceName $countPasses"> ./Testing/${folder}/info.txt
+#echo "$pcName $instanceName $countPasses"> ./Testing/${folder}/info.txt
 ./Testing/killHung.sh ${folder} &
 for k in ${kList[@]}; do
     for n in ${nList[@]}; do
 
 
-        echo "$k $instanceFirstLine $n" > ./instances/${instanceName}${k}.csv
-        cat ./instances/${instanceName}K.csv >> ./instances/${instanceName}${k}.csv
-        >> ./Testing/${folder}/times-${pcName}-${k}-${n}.txt
-        > ./Testing/${folder}/out.txt
-        for w in ${wList[@]}; do #((w = 158 ; w < 161 ; ++w));
-            # v=3 
-            #PATH=build/:$PATH RDMAV_FORKSAVE=1 mpirun -np ${w} -oversubscribe build/mallob -mono-application=KMEANS -mono=./instances/${instanceName}${k}.csv -v=3
-            # v=0 >> ./Testing/times-${k}.txt 
-            meanTime=0
-            for ((i = 0 ; i < $countPasses ; ++i)); do
-                > ./Testing/${folder}/out.txt
-                PATH=build/:$PATH RDMAV_FORKSAVE=1 mpirun -np ${w} -oversubscribe build/mallob -mono-application=KMEANS -mono=./instances/${instanceName}${k}.csv -v=2 2>&1 > ./Testing/${folder}/out.txt
-                meanTime=$(echo "scale=3; $meanTime + ($(cat ./Testing/${folder}/out.txt |grep "Got Result"|awk '{print $2}'))" | bc)
-                #echo "time $(cat ./Testing/${folder}/out.txt |grep "Got Result"|awk '{print $2}')"
-            done
-            meanTime=$(echo "scale=3; $meanTime / $countPasses" | bc)
-            #echo "$w $meanTime"
-            echo "k=$k n=$n w=$w iters=$(cat ./Testing/${folder}/out.txt |grep "Got Result"|awk '{print $8}')" >> ./Testing/${folder}/info.txt
-            echo "$w $meanTime" >> ./Testing/${folder}/times-${pcName}-${k}-${n}.txt
-            #cat ./Testing/${folder}/out.txt |grep "Got Result"
-        done
-        rm ./instances/${instanceName}${k}.csv
+      #  echo "$k $instanceFirstLine $n" > ./instances/${instanceName}${k}.csv
+      #  cat ./instances/${instanceName}K.csv >> ./instances/${instanceName}${k}.csv
+      #  >> ./Testing/${folder}/times-${pcName}-${k}-${n}.txt
+      #  > ./Testing/${folder}/out.txt
+      #  for w in ${wList[@]}; do #((w = 158 ; w < 161 ; ++w));
+      #      # v=3 
+      #      #PATH=build/:$PATH RDMAV_FORKSAVE=1 mpirun -np ${w} -oversubscribe build/mallob -mono-application=KMEANS -mono=./instances/${instanceName}${k}.csv -v=3
+      #      # v=0 >> ./Testing/times-${k}.txt 
+      #      meanTime=0
+      #      for ((i = 0 ; i < $countPasses ; ++i)); do
+      #          > ./Testing/${folder}/out.txt
+      #          PATH=build/:$PATH RDMAV_FORKSAVE=1 mpirun -np ${w} -oversubscribe build/mallob -mono-application=KMEANS -mono=./instances/${instanceName}${k}.csv -v=2 2>&1 > ./Testing/${folder}/out.txt
+      #          meanTime=$(echo "scale=3; $meanTime + ($(cat ./Testing/${folder}/out.txt |grep "Got Result"|awk '{print $2}'))" | bc)
+      #          #echo "time $(cat ./Testing/${folder}/out.txt |grep "Got Result"|awk '{print $2}')"
+      #      done
+      #      meanTime=$(echo "scale=3; $meanTime / $countPasses" | bc)
+      #      #echo "$w $meanTime"
+      #      echo "k=$k n=$n w=$w iters=$(cat ./Testing/${folder}/out.txt |grep "Got Result"|awk '{print $8}')" >> ./Testing/${folder}/info.txt
+      #      echo "$w $meanTime" >> ./Testing/${folder}/times-${pcName}-${k}-${n}.txt
+      #      #cat ./Testing/${folder}/out.txt |grep "Got Result"
+      #  done
+      #  rm ./instances/${instanceName}${k}.csv
 
 
         t1=$(cat ./Testing/${folder}/times-${pcName}-${k}-${n}.txt | grep "^1 " | awk '{print $2}')
