7e74ff3617ebdef3935ec10270199fe38b774efe
Michael Doerr <michael@midoerr.de>
2022-09-09 02:28:21 +0200

    results

--- Testing/orchestrated.sh
+++ Testing/orchestrated.sh
@@ -9,14 +9,14 @@ jobTemplate="templates/job-template-kmeans.json"
 clientTemplate="templates/client-template-KR.json"
 v="2"
 demand="Restricted" #Unrestricted Restricted
-time="40"
+time="60"
 
 mkdir -p ./Testing/${folder}
 echo "pcName:$pcName np:$np J:$J ajpc:$ajpc $jobDescTemplate $jobTemplate $clientTemplate v:$v time:$time"> ./Testing/${folder}/info${demand}.txt
 
 
-#./Testing/killAfter.sh ${time} &
-#PATH=build/:$PATH RDMAV_FORKSAVE=1 mpirun -np $np --use-hwthread-cpus --map-by numa:PE=2 --bind-to hwthread build/mallob -c=1 -J=$J -ajpc=$ajpc -job-desc-template=$jobDescTemplate -job-template=$jobTemplate -client-template=$clientTemplate -pls=0 -v=$v 2>&1 > ./Testing/${folder}/out${demand}.txt
+./Testing/killAfter.sh ${time} &
+PATH=build/:$PATH RDMAV_FORKSAVE=1 mpirun -np $np --use-hwthread-cpus --map-by numa:PE=2 --bind-to hwthread build/mallob -c=1 -J=$J -ajpc=$ajpc -job-desc-template=$jobDescTemplate -job-template=$jobTemplate -client-template=$clientTemplate -pls=0 -v=$v 2>&1 > ./Testing/${folder}/out${demand}.txt
     
 cat ./Testing/${folder}/out${demand}.txt |grep "Got Result"|awk '{print $1}' > ./Testing/${folder}/plain-runtimes${demand}.txt
 cat ./Testing/${folder}/plain-runtimes${demand}.txt | sort -g | awk '{print $1,NR}' > ./Testing/${folder}/cdf-runtimes${demand}.txt
