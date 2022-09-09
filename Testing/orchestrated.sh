51cb5dd5ade1b89865ab407f541f49c11ff43733
Michael Doerr <michael@midoerr.de>
2022-09-09 02:38:24 +0200

    test prepared

--- Testing/orchestrated.sh
+++ Testing/orchestrated.sh
@@ -8,8 +8,8 @@ jobDescTemplate="instances/kmeansTest"
 jobTemplate="templates/job-template-kmeans.json" 
 clientTemplate="templates/client-template-KR.json"
 v="2"
-demand="Unrestricted" #Unrestricted Restricted
-time="60"
+demand="Restricted" #Unrestricted Restricted
+time="3000"
 
 mkdir -p ./Testing/${folder}
 echo "pcName:$pcName np:$np J:$J ajpc:$ajpc $jobDescTemplate $jobTemplate $clientTemplate v:$v time:$time"> ./Testing/${folder}/info${demand}.txt
--- Testing/plotClickDemand.cmd
+++ Testing/plotClickDemand.cmd
@@ -1,14 +1,13 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
-set folder=nTest256core100d150exact
+set folder=maxDemandDefaultTest
 set pcName=i10pc138
+set cm=python ../scripts/plot/plot_curves.py -xy -xlabel="seconds" -ylabel="solved jobs"
+
 for %%t in (Restricted Unrestricted) do (
-    
-        set cm=python ../scripts/plot/plot_curves.py -xy -xlabel="seconds" -ylabel="solved jobs"
-        
-        set cm=!cm! .\%folder%\cdf-runtimes%%t.txt -l="%%t"
-
-        set cm=!cm!  -o=.\%folder%\Graph.pdf
-        call !cm!
-
+    set cm=!cm! .\%folder%\cdf-runtimes%%t.txt -l="%%t"
 )
+
+set cm=!cm!  -o=.\%folder%\Graph.pdf
+call !cm!
+
 pause
