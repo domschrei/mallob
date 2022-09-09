54d92fdf4426daf1c46c01ee7330007fe7d58871
Michael Doerr <michael@midoerr.de>
2022-09-09 14:44:46 +0200

    results

--- Testing/orchestrated.sh
+++ Testing/orchestrated.sh
@@ -8,7 +8,7 @@ jobDescTemplate="instances/kmeansTest"
 jobTemplate="templates/job-template-kmeans.json" 
 clientTemplate="templates/client-template-KR.json"
 v="2"
-demand="Restricted" #Unrestricted Restricted
+demand="Unrestricted" #Unrestricted Restricted
 time="3000"
 
 mkdir -p ./Testing/${folder}
