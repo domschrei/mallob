19bf0310226a44c9fd608d9ebbbddc432e501b5e
Michael Doerr <michael@midoerr.de>
2022-09-11 00:34:11 +0200

    results

--- Testing/orchestrated138.sh
+++ Testing/orchestrated138.sh
@@ -3,12 +3,12 @@ pcName="i10pc138"
 folder="maxDemandDefaultTest138Version5" #latestResults
 np="128"
 J="10000"
-ajpc="9"
+ajpc="8"
 jobDescTemplate="instances/kmeansTest" 
 jobTemplate="templates/job-template-kmeans.json" 
 clientTemplate="templates/client-template-KR.json"
 v="2"
-demand="Restricted" #Unrestricted Restricted
+demand="Unrestricted" #Unrestricted Restricted
 time="200"
 
 mkdir -p ./Testing/${folder}
