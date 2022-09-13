369f944d2acaebb192f8a72207cec9a9ecb8e7f1
Michael Doerr <ucvuu@student.kit.edu>
2022-09-13 23:07:26 +0200

    plot

--- Testing/progressiveKMeans.py
+++ Testing/progressiveKMeans.py
@@ -9,15 +9,15 @@ from sklearn.neighbors import kneighbors_graph
 from sklearn.preprocessing import StandardScaler
 from itertools import cycle, islice
 
-np.random.seed(0)
+np.random.seed(2)
 
 
 # blobs with varied variances
-n_samples = 500
-random_state = 170
-blobs = datasets.make_blobs(n_samples=n_samples, random_state=8)
+n_samples = 200
+random_state = 40
+#blobs = datasets.make_blobs(n_samples=n_samples, random_state=8)
 varied = datasets.make_blobs(
-    n_samples=n_samples, cluster_std=[1.0, 2.5, 0.5], random_state=random_state
+    n_samples=n_samples, cluster_std=[1.0, 2.5, 0.7], random_state=random_state
 )
 
 # ============
@@ -100,7 +100,7 @@ for i_dataset, (dataset, algo_params) in enumerate(datasets):
     )
 
     i=1
-    maxIter=2
+    maxIter=1
     for name, algorithm in clustering_algorithms:
         t0 = time.time()
         algorithm = cluster.KMeans(n_clusters=params["n_clusters"], max_iter=maxIter,n_init=1, init=(centroids if centroids is not None else 'random'))
@@ -173,6 +173,11 @@ for i_dataset, (dataset, algo_params) in enumerate(datasets):
         plt.scatter(X[:, 0][y_pred==0], X[:, 1][y_pred==0], s=300, color="#377eb8", marker="^")
         plt.scatter(X[:, 0][y_pred==1], X[:, 1][y_pred==1], s=300, color="#ff7f00", marker="s")
         plt.scatter(X[:, 0][y_pred==2], X[:, 1][y_pred==2], s=300, color="#4daf4a", marker="o")
+
+        
+        plt.scatter(centroids[0, 0], centroids[0, 1], s=500, color="#000000", marker="^")
+        plt.scatter(centroids[1, 0], centroids[1, 1], s=500, color="#000000", marker="s")
+        plt.scatter(centroids[2, 0], centroids[2, 1], s=500, color="#000000", marker="o")
 
         plt.xlim(-2.5, 2.5)
         plt.ylim(-2.5, 2.5)
