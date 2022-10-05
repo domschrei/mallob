520508b693edc3b5c88e71af9564112c4f4d2098
Dominik Schreiber <mail@dominikschreiber.de>
2022-10-05 16:05:39 +0200

    Fix preliminary timing of LRAT decompression

--- docker/leader/proof_check.sh
+++ docker/leader/proof_check.sh
@@ -4,7 +4,7 @@
 time1=$(date +"%s.%3N")
 /decompress /logs/processes/renumbered.lrat > /logs/processes/renumbered-dec.lrat
 time2=$(date +"%s.%3N")
-echo "Decompressed proof ($(echo "$time2 - $time1"|bc -l)s)" # TODO add proper timing string
+echo "Decompressed proof ($(echo $time1 $time2|awk '{print $2-$1}')s)" # TODO add proper timing string
 
 # Check the decompressed proof
 /lrat-check $1 /logs/processes/renumbered-dec.lrat
