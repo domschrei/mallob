a6da8233ee1f4908014713978a5a9e9fd7917ae1
Michael Doerr <michael@midoerr.de>
2022-10-25 23:00:16 +0200

    cleanup

--- Testing/killAfter.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sleep ${1}
-killall -9 mpirun ; killall -9 build/mallob
--- Testing/plot.cmd
+++
@@ -1,6 +0,0 @@
-
-for %%k in (5 10 20 30 40) do (
-    python scripts/plot/plot_curves.py -xy ./Testing/times-%%k.txt -l="k=%%k" -o=./Testing/Graph%%k.pdf
-)
-
-
