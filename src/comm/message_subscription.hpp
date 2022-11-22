814a7c7d19cb7dcc9c4f15df3ca635e435d439d4
Dominik Schreiber <mail@dominikschreiber.de>
2022-11-22 15:28:17 +0100

    Documentation for MessageSubscription

--- src/comm/message_subscription.hpp
+++ src/comm/message_subscription.hpp
@@ -4,6 +4,12 @@
 #include "mympi.hpp"
 #include "message_queue.hpp"
 
+// RAII structure for a subscription to incoming messages of a certain tag.
+// Subscriptions are non-exclusive, i.e., there can be multiple subscriptions
+// for a given tag at the same time. Whenever a message of the concerned tag
+// arrives, the supplied callback is called for each active subscription.
+// The subscription is cleared upon destruction of the MessageSubscription 
+// object.
 class MessageSubscription {
 
 private:
@@ -11,10 +17,16 @@ private:
     MessageQueue::CallbackRef _cb_ref;
 
 public:
+    // @param tag the message tag to listen to (see msgtags.h)
+    // @param callback the function to call whenever a message with the
+    // supplied tag arrives
     MessageSubscription(int tag, MessageQueue::MsgCallback callback) : _tag(tag) {
         _cb_ref = MyMpi::getMessageQueue().registerCallback(tag, callback);
     }
     
+    // @param tag the message tag to listen to (see msgtags.h)
+    // @param callback the function to call whenever a message with the
+    // supplied tag arrives
     MessageSubscription(int tag, void (*callback)(MessageHandle&)) : _tag(tag) {
         _cb_ref = MyMpi::getMessageQueue().registerCallback(tag, [&, callback](auto& h) {
             callback(h);
@@ -26,7 +38,6 @@ public:
         _cb_ref = std::move(moved._cb_ref);
         moved._tag = -1;
     }
-
     MessageSubscription& operator=(MessageSubscription&& moved) {
         _tag = moved._tag;
         _cb_ref = std::move(moved._cb_ref);
