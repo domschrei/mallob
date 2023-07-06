0591de30b5c5c2ffb4f92c2111cbed19373105d4
Dominik Schreiber <mail@dominikschreiber.de>
2023-07-06 09:34:26 +0200

    Activate init. var shuffling randomly with increasing probability

--- src/app/sat/solvers/kissat.cpp
+++ src/app/sat/solvers/kissat.cpp
@@ -1,4 +1,5 @@
 
+#include "util/random.hpp"
 #include "util/sys/timer.hpp"
 #include "util/tsl/robin_set.h"
 
@@ -136,29 +137,37 @@ void Kissat::diversify(int seed) {
     // Diversify the order in which the variables are activated.
     // This is done by generating a pseudo-random permutation from 0 to #vars-1
     // and then activating the variables in this order.
-    if (_setup.diversifyInitShuffle) {
-        float time = Timer::elapsedSeconds();
-        kissat_set_option(solver, "manualvaractivation", 1);
-        AdjustablePermutation perm(getSolverSetup().numVars, seed);
-        // With heavy assertions enabled, actually track that all variables
-        // have been activated in the end.
-#if MALLOB_ASSERT == 2
-        tsl::robin_set<int> activatedVars;
-#endif
-        for (size_t i = 0; i < getSolverSetup().numVars; i++) {
-            int var = perm.get(i, false)+1;
-            kissat_activate_variable(solver, var);
-#if MALLOB_ASSERT == 2
-            activatedVars.insert(var);
-#endif
+    if (getDiversificationIndex() >= getNumOriginalDiversifications() && _setup.diversifyInitShuffle) {
+        SplitMix64Rng rng(seed);
+        float k = 0.1;
+        float shufProbability = 1 - 0.8 * std::exp(-k * (int) (getDiversificationIndex() / getNumOriginalDiversifications() - 1));
+        auto r = rng.randomInRange(0, 1);
+        LOGGER(_logger, V3_VERB, "Init var shuffle decision: %.4f < %.4f\n", r, shufProbability);
+        if (r < shufProbability) {
+            // Shuffle!
+            float time = Timer::elapsedSeconds();
+            kissat_set_option(solver, "manualvaractivation", 1);
+            AdjustablePermutation perm(getSolverSetup().numVars, seed);
+            // With heavy assertions enabled, actually track that all variables
+            // have been activated in the end.
+    #if MALLOB_ASSERT == 2
+            tsl::robin_set<int> activatedVars;
+    #endif
+            for (size_t i = 0; i < getSolverSetup().numVars; i++) {
+                int var = perm.get(i, false)+1;
+                kissat_activate_variable(solver, var);
+    #if MALLOB_ASSERT == 2
+                activatedVars.insert(var);
+    #endif
+            }
+    #if MALLOB_ASSERT == 2
+            LOGGER(_logger, V3_VERB, "Checking activated vars\n");
+            assert(activatedVars.size() == getSolverSetup().numVars);
+    #endif
+            time = Timer::elapsedSeconds() - time;
+            LOGGER(_logger, V3_VERB, "Init var shuffle took %.3fs\n", time);
         }
-#if MALLOB_ASSERT == 2
-        LOGGER(_logger, V3_VERB, "Checking activated vars\n");
-        assert(activatedVars.size() == getSolverSetup().numVars);
-#endif
-        time = Timer::elapsedSeconds() - time;
-        LOGGER(_logger, V3_VERB, "Init var shuffle took %.3fs\n", time);
-    }
+        }
 
     seedSet = true;
     setClauseSharing(getNumOriginalDiversifications());
