e5c5a03c25cde56202a108272a71b0c2b90f619e
nrilu <11998876+nrilu@users.noreply.github.com>
2025-03-25 17:05:19 +0100

    Fix Triangle size=2 lbd=3 error

--- src/app/sat/solvers/portfolio_solver_interface.cpp
+++ src/app/sat/solvers/portfolio_solver_interface.cpp
@@ -105,24 +105,33 @@ PortfolioSolverInterface::PortfolioSolverInterface(const SolverSetup& setup)
 			assert(c.lbd <= c.size-ClauseMetadata::numInts());
 		}
 		else if (_setup.randomizeLbdBeforeImport==2) {
-			//Triangle sample 
+			//Triangle sample
 			int truesize = c.size - ClauseMetadata::numInts();
-			//If the clause size is 1, 2 or 3 we dont have options, we always assign at least lbd 3
-			int lbd_options= std::max(1,truesize-2);
-			//For larger clauses we use a triangular distribution to more weight to higher lbd values
-			//We sample from a rectangle by stacking two triangles above each other (one flippe)
-			// lbd_start: the "column" of the rectangle, range [0,lbd_options-1]
-			// flip:	  the "row" of the rectangle,    range [0,lbd_option]
-			//if flip lands in the "flipped" triangle region, we mirror the lbd around the center
-			//This works because each pair of columns (mirrored around the center) have the same summed weight
-			int lbd_column = (int) std::round(_rng.randomInRange(0 - 0.49999,(lbd_options-1) + 0.49999));
-			int flip = (int) std::round(_rng.randomInRange(0 - 0.49999, lbd_options    + 0.49999));
-			//A large enough j "mirrors" the lbd to the other end of the triangle distribution
-			if(lbd_column <= flip) lbd_column = (lbd_options-1) - lbd_column;
-			//Our distribution was calculated the range [0,...] so now we transform it into [3,...] for have always lbd=>3
-			c.lbd = lbd_column+3;
-			//cout << "eff_size="<<(c.size-ClauseMetadata::numInts())<<endl;
-			//cout <<" trig_lbd="<<c.lbd<<endl;
+			//Special cases:
+			//Size=1: Unit clause, want to keep lbd=1
+			//Size=2: Set lbd=2, otherwise lbd=3 would cause assertion failure
+			//Size=3: Set lbd=3, because we want to bump all lbds (if possible) to at least 3
+			if(truesize<=3) {
+				c.lbd=truesize;
+			}
+			else {
+				//Sizes >=4: Choose a random lbd from [3,..,size], sapled form a triangular distribution, i.e. higher lbds are more likely
+				int lbd_options= std::max(1,truesize-2);
+				//For larger clauses we use a triangular distribution to more weight to higher lbd values
+				//We sample from a rectangle by stacking two triangles above each other (one flippe)
+				// lbd_start: the "column" of the rectangle, range [0,lbd_options-1]
+				// flip:      the "row" of the rectangle,    range [0,lbd_option]
+				//if flip lands in the "flipped" triangle region, we mirror the lbd around the center
+				//This works because each pair of columns (mirrored around the center) have the same summed weight
+				int lbd_column = (int) std::round(_rng.randomInRange(0 - 0.49999,(lbd_options-1) + 0.49999));
+				int flip = (int) std::round(_rng.randomInRange(0 - 0.49999, lbd_options    + 0.49999));
+				//A large enough j "mirrors" the lbd to the other end of the triangle distribution
+				if(lbd_column <= flip) lbd_column = (lbd_options-1) - lbd_column;
+				//Our distribution was calculated the range [0,...] so now we transform it into [3,...] for have always lbd=>3
+				c.lbd = lbd_column+3;
+				//cout << "eff_size="<<(c.size-ClauseMetadata::numInts())<<endl;
+				//cout <<" trig_lbd="<<c.lbd<<endl;
+			}
 		}
 
 		if (_setup.incrementLbdBeforeImport && c.lbd < c.size-ClauseMetadata::numInts())
