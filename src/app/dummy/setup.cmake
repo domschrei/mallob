17d68d35814aa283af9d2933986f18f7f2f68ce3
Dominik Schreiber <mail@dominikschreiber.de>
2025-07-24 16:38:23 +0200

    Improve build process, reduce libraries linked/compiled into sub-process executables

--- src/app/dummy/setup.cmake
+++ src/app/dummy/setup.cmake
@@ -3,7 +3,7 @@
 set(DUMMY_SOURCES src/app/dummy/dummy_reader.cpp)
 
 # Add these sources to Mallob's base sources
-set(MALLOB_COREPLUSCOMM_SOURCES ${MALLOB_COREPLUSCOMM_SOURCES} ${DUMMY_SOURCES} CACHE INTERNAL "")
+set(MALLOB_MAINAPP_SOURCES ${MALLOB_MAINAPP_SOURCES} ${DUMMY_SOURCES} CACHE INTERNAL "")
 
 #message("commons+DUMMY sources: ${BASE_SOURCES}") # Use to debug
 
--- src/app/satwithpre/setup.cmake
+++ src/app/satwithpre/setup.cmake
@@ -1,7 +1,10 @@
 
 # Add MaxSAT-specific sources to main Mallob executable
 set(SATWITHPRE_MALLOB_SOURCES src/app/sat/solvers/kissat.cpp src/app/sat/solvers/lingeling.cpp src/app/sat/solvers/portfolio_solver_interface.cpp)
-set(MALLOB_COREPLUSCOMM_SOURCES ${MALLOB_COREPLUSCOMM_SOURCES} ${SATWITHPRE_MALLOB_SOURCES} CACHE INTERNAL "")
+set(MALLOB_MAINAPP_SOURCES ${MALLOB_MAINAPP_SOURCES} ${SATWITHPRE_MALLOB_SOURCES} CACHE INTERNAL "")
+
+# Need to explicitly link used SAT solver libraries
+set(BASE_LIBS ${BASE_LIBS} lgl yals cadical kissat CACHE INTERNAL "")
 
 #message("commons+SAT sources: ${BASE_SOURCES}") # Use to debug
 
