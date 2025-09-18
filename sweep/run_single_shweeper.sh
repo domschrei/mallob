d124482013e467c0e75f5dc8afffb3cc17d432b5
nrilu <11998876+nrilu@users.noreply.github.com>
2025-09-18 15:16:01 +0200

    clean single-threaded shweeper, no leaking there...

--- sweep/run_single_shweeper.sh
+++ sweep/run_single_shweeper.sh
@@ -2,10 +2,14 @@
 
 INST_DIR="$HOME/PhD/instances/miter/18faad09a2e931cdfb4c8d7b1f2ef35f-rotmul.miter.used-as.sat04-336.cnf"
 
-~/PhD/ksst-sweep/kissat/build/kissat --mallob_is_shweeper=1 \
+OPTS="--mallob_is_shweeper=1 \
   --sweepcomplete=1 \
   --probe=1 \
   --verbose=1 \
   --mallob_custom_sweep_verbosity=2 \
   --check=0 \
-  $INST_DIR
+  $INST_DIR"
+
+# ~/PhD/ksst-sweep/kissat/build/kissat $OPTS
+# valgrind ~/PhD/ksst-sweep/kissat/build/kissat $OPTS
+valgrind --leak-check=full ~/PhD/ksst-sweep/kissat/build/kissat $OPTS
