e25468da3c55ef98a742c809e9f2d83e3ad3ad78
nrilu <11998876+nrilu@users.noreply.github.com>
2025-10-13 16:59:21 +0200

    integrating slurm scripts in repo

--- scripts/slurm/generate-job-chain.sh
+++ scripts/slurm/generate-job-chain.sh
@@ -2,25 +2,27 @@
 
 source $ACCOUNTINFO # $projectname , $username
 
-sbatch_base="sbatch.sh"
+echo "$username"
+echo "$projectname"
+
+sbatch_base="scripts/slurm/sbatch.sh"
 minjobidx="$1"
 maxjobidx="$2"
 numchains="$3"
 benchmarkfile="dummy"
 jobname="$4"
 
-cd $HOME/mallob
+#cd $HOME/mallob
 echo ""
 echo "$jobname: sbatch_base $sbatch_base"
 echo "$jobname: min         $minjobidx"
 echo "$jobname: max         $maxjobidx"
 echo "$jobname: chains      $numchains"
 echo "$jobname: nodes       $DS_NODES"
-#echo "benchmarkf  $benchmarkfile"
 echo " "
 
 #As security check show the current mallob flags
-lcmd.sh
+scripts/slurm/showflags.sh
 echo " "
 
 dir="$HOME/mallob/sbatch/generated/$jobname"
@@ -29,6 +31,8 @@ mkdir -p "$dir"
 out_templated="$dir/sbatch.sh"
 runtime_slurmstr=$(date -d@${DS_RUNTIME} -u +%H:%M:%S)
 
+echo "templated origin: $sbatch_base"
+echo "templated target: $out_templated"
 cp "$sbatch_base" "$out_templated"
 
 sed -i 's/$DS_PROJECTNAME/'$projectname'/g' "$out_templated"
@@ -46,8 +50,7 @@ sed -i "s/\$DS_BENCHMARKFILE/$escaped_benchmarkfile/g" "$out_templated"
 
 cmd="for i in {1..$numchains}; do sbatch $out_templated; done"
 
-#echo "Execute the following command:"
+echo "Execute the following command:"
+echo ""
 echo "$cmd"
 echo ""
-
-~          
--- scripts/slurm/lcmd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Sanity check: Show the current mallob runtime flags
-echo $(grep "^benchmarkfile" "sbatch.sh") 
-flags=$(grep -A 12 -- "-mono" "sbatch.sh")
-echo "$flags"
-
