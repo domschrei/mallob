e42ae191cd178354f0c0c32a844c31807aa661da
nrilu <11998876+nrilu@users.noreply.github.com>
2025-10-13 15:33:36 +0200

    server slurm change

--- scripts/slurm/generate-job-chain.sh
+++ scripts/slurm/generate-job-chain.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-source scripts/slurm/account.sh # $projectname , $username
+source $ACCOUNTINFO # $projectname , $username
 
-jobname="$1"
-sbatch_base="$2"
-minjobidx="$3"
-maxjobidx="$4"
-numchains="$5"
+sbatch_base="sbatch.sh"
+minjobidx="$1"
+maxjobidx="$2"
+numchains="$3"
+benchmarkfile="dummy"
+jobname="$4"
 
-dir="sbatch/generated/$jobname"
+cd $HOME/mallob
+echo ""
+echo "$jobname: sbatch_base $sbatch_base"
+echo "$jobname: min         $minjobidx"
+echo "$jobname: max         $maxjobidx"
+echo "$jobname: chains      $numchains"
+echo "$jobname: nodes       $DS_NODES"
+#echo "benchmarkf  $benchmarkfile"
+echo " "
+
+#As security check also show the current mallob flags
+lcmd.sh
+#mallob_cmd=$(grep -A 12 -- "-mono" "$NEWSCRIPTS/sbatch.sh")
+#echo "$mallob_cmd"
+echo " "
+
+
+dir="$HOME/mallob/sbatch/generated/$jobname"
 mkdir -p "$dir"
 
 out_templated="$dir/sbatch.sh"
@@ -26,9 +44,13 @@ sed -i 's/$DS_PARTITION/'$DS_PARTITION'/g' "$out_templated"
 sed -i 's/$DS_SECONDSPERJOB/'$DS_SECONDSPERJOB'/g' "$out_templated"
 sed -i 's/$DS_FIRSTJOBIDX/'$minjobidx'/g' "$out_templated"
 sed -i 's/$DS_LASTJOBIDX/'$maxjobidx'/g' "$out_templated"
+escaped_benchmarkfile=$(echo "$benchmarkfile" | sed 's/\//\\\//g')
+sed -i "s/\$DS_BENCHMARKFILE/$escaped_benchmarkfile/g" "$out_templated"
 
 cmd="for i in {1..$numchains}; do sbatch $out_templated; done"
 
-echo "Execute the following command:"
+#echo "Execute the following command:"
 echo "$cmd"
+echo ""
 
+~          
