a31cb3e081ddef30f7afea2ee70a386728b906f8
nrilu <11998876+nrilu@users.noreply.github.com>
2025-10-13 14:58:05 +0200

    expanded slurm build script

--- scripts/setup/build.sh
+++ scripts/setup/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
+#
+# TODO Load your own suitable modules here
+module load slurm_setup
+module unload devEnv/Intel/2019 intel-mpi cmake
+module load gcc/11 intel-mpi/2019-gcc cmake/3.21.4 gdb valgrind
+echo "Modules loaded"
+#
 # Only needed if building with -DMALLOB_APP_SAT=1 (enabled by default).
 # For non-x86-64 architectures (ARM, POWER9, etc.), prepend `DISABLE_FPU=1` to "bash".
 ( cd lib && bash fetch_and_build_solvers.sh k sweep)
@@ -8,13 +14,18 @@
 # Specify `-DCMAKE_BUILD_TYPE=RELEASE` for a release build or `-DCMAKE_BUILD_TYPE=DEBUG` for a debug build.
 # Find all build options at: docs/setup.md
 mkdir -p build
+rm build/*mallob*
+
 cd build
 CC=$(which mpicc) CXX=$(which mpicxx) cmake -DCMAKE_BUILD_TYPE=RELEASE \
   -DMALLOB_APP_SAT=1 \
+  -DMALLOB_APP_SATWITHPRE=1 \
+  -DMALLOB_APP_SWEEP=1 \
   -DMALLOB_LOG_VERBOSITY=4 \
   -DMALLOB_ASSERT=1 \
   -DMALLOB_USE_JEMALLOC=1 \
-  -DMALLOB_SUBPROC_DISPATCH_PATH=\"build/\" ..
+  -DMALLOB_SUBPROC_DISPATCH_PATH=\"build/\" .. \
+  -DMALLOB_MAX_N_APPTHREADS_PER_PROCESS=64
 
 make clean
 make -j 20; cd ..
