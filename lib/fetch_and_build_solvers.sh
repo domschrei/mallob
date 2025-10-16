b61588269b380d6338595f1d03c26d9b4bf573ee
nrilu <11998876+nrilu@users.noreply.github.com>
2025-10-16 18:11:53 +0200

    fixed mismatched regex on gcc on server, gave -O instead of -O3 !

--- lib/fetch_and_build_solvers.sh
+++ lib/fetch_and_build_solvers.sh
@@ -101,7 +101,9 @@ if echo $solvers|grep -q "k" && [ ! -f kissat/libkissat.a ]; then
     echo "Building Kissat ..."
 
     cd kissat
-    ./configure --no-proofs
+    ##Need manual -O3 on the server!
+    ##Because there ./configure doesn't regex-match the gcc compiler, and falls back to only -O 
+    ./configure -O3
     make -j 16
     cp build/libkissat.a .
     cd ..
