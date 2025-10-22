5efef0618d181de3444550aaebb98fa4631d1a5a
Dominik Schreiber <mail@dominikschreiber.de>
2025-10-22 14:16:04 +0200

    Outsource fetching of SMT deps

---
+++ lib/fetch_smt_deps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -d bitwuzla ]; then
+    if [ ! -f bitwuzla.zip ]; then
+        # for fixing a branch instead of a commit, prepend "refs/heads/"
+        branchorcommit="5e1211dc06a9f61fa96f1ea4d1e433849d8e67ae" # main branch, updated 2025-10-09
+        wget -nc https://github.com/domschrei/bitwuzla/archive/${branchorcommit}.zip -O bitwuzla.zip
+    fi
+    unzip bitwuzla.zip
+    mv bitwuzla-* bitwuzla
+fi
