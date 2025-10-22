7ff57457c91f6605aa71b5c6aa692c29b3a1b53b
nrilu <11998876+nrilu@users.noreply.github.com>
2025-10-22 17:50:14 +0200

    minimum sharing period only 1ms

--- src/app/sweep/options.hpp
+++ src/app/sweep/options.hpp
@@ -6,5 +6,5 @@
 // Add your application-specific options here
 
 OPTION_GROUP(grpAppSweep, "app/sweep", "Options for application sweep")
-OPT_INT(sweepSharingPeriod_ms, "ssp", "sweep-sharing-period", 250, 10, LARGE_INT, "The period (in ms) between initiating Equivalence+Units sharing operations")
+OPT_INT(sweepSharingPeriod_ms, "ssp", "sweep-sharing-period", 250, 1, LARGE_INT, "The period (in ms) between initiating sharing operations of Equivalences and Units")
 OPT_INT(sweepSolverVerbosity, "", "sweep-solver-verbosity", 0, 0, 5, "verbosity of the kissat sweeping solvers in the SWEEP app")
