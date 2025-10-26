43290074677b937cb471f9006f71bf2ebfd86948
Dominik Schreiber <mail@dominikschreiber.de>
2025-10-26 11:16:21 +0100

    Update Bitwuzla

--- lib/fetch_smt_deps.sh
+++ lib/fetch_smt_deps.sh
@@ -5,7 +5,7 @@ set -e
 if [ ! -d bitwuzla ]; then
     if [ ! -f bitwuzla.zip ]; then
         # for fixing a branch instead of a commit, prepend "refs/heads/"
-        branchorcommit="40489f8f16c7c467e8f246a7201ddea8885e4daa" # external-sat-solver branch, updated 2025-10-23
+        branchorcommit="35a785c183e07b98d0c426e7cf0e8a4cf78a685b" # external-sat-solver branch, updated 2025-10-26
         wget -nc https://github.com/bitwuzla/bitwuzla/archive/${branchorcommit}.zip -O bitwuzla.zip
     fi
     unzip bitwuzla.zip
