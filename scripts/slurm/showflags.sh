7fe28da513dce3ff27f2537eb444364ac6c6e284
nrilu <11998876+nrilu@users.noreply.github.com>
2026-01-09 16:36:26 +0100

    showflags of compile

--- scripts/slurm/showflags.sh
+++ scripts/slurm/showflags.sh
@@ -2,9 +2,21 @@
 #Sanity check: Show the current mallob runtime flags
 # echo $(grep "^benchmarkfile" "scripts/slurm/sbatch.sh") 
 # echo ""
-echo ""
+# echo ""
+set -e
 # flags=$(grep -A 18 -- "build/mallob" "scripts/slurm/sbatch.sh")
-flags=$(grep -A 25 -- "build/mallob" "$1")
+# flags=$(grep -A 25 -- "build/mallob" "$1")
+# echo "$flags"
+# echo ""
+
+build_flags=$(grep -A 25 -- "if \[" "sweep/build_on_supermuc.sh")
+echo "$build_flags"
+echo "----------------------------------------------------------------------------"
+echo ""
+echo "----------------------------------------------------------------------------"
+provided_sbatch_file=$1
+flags=$(grep -A 21 -- "build/mallob" "$provided_sbatch_file")
 echo "$flags"
+echo "----------------------------------------------------------------------------"
 echo ""
 
