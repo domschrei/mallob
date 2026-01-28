2d058ad091fd6330e1cc41669f7a96717d0a7342
Dominik Schreiber <mail@dominikschreiber.de>
2026-01-28 12:57:25 +0100

    Include ImpCheck in sat-setup.sh

--- scripts/setup/sat-setup.sh
+++ scripts/setup/sat-setup.sh
@@ -3,6 +3,15 @@
 set -e
 
 cd lib
-bash fetch_and_build_solvers.sh kcly
+  bash fetch_and_build_solvers.sh kcly
+
+  if [ ! -d impcheck ]; then git clone git@github.com:domschrei/impcheck.git ; fi
+  cd impcheck
+    git checkout incremental
+    mkdir -p build ; cd build
+      cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DIMPCHECK_WRITE_DIRECTIVES=0 -DIMPCHECK_FLUSH_ALWAYS=0 -DIMPCHECK_COMPRESS=1 ; make -j
+    cd ..
+  cd ..
+  cp impcheck/build/impcheck_* ../build/
 cd ..
 
