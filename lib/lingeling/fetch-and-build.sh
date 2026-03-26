62b69ec95e9a280a2ce9e5caef961a29c9b2fc13
Dominik Schreiber <mail@dominikschreiber.de>
2026-03-26 18:39:38 +0100

    Reduce Lingeling and YalSAT re-builds

--- lib/lingeling/fetch-and-build.sh
+++ lib/lingeling/fetch-and-build.sh
@@ -6,9 +6,11 @@ dirname="lingeling"
 branchorcommit="89a167d0d2efe98d983c87b5b84175b40ea55842" # version 1.0.0, March 2024
 fetch_and_extract $dirname configure.sh https://github.com/arminbiere/lingeling/archive/${branchorcommit}.zip
 
-for f in *.c *.h ; do
-    sed -i 's/exit ([01])/abort()/g' $f
-done
+if grep -qE "exit \([01]\)" *.c *.h ; then
+    for f in *.c *.h ; do
+        sed -i 's/exit ([01])/abort()/g' $f
+    done
+fi
 
 echo "[$dirname] Building ..."
 ./configure.sh
