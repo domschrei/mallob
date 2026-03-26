e5668971e387cd85370f9642dd776baaba5f5286
Dominik Schreiber <mail@dominikschreiber.de>
2026-03-26 18:42:48 +0100

    Reduce Lingeling and YalSAT re-builds

--- lib/lingeling/fetch-and-build.sh
+++ lib/lingeling/fetch-and-build.sh
@@ -7,12 +7,12 @@ branchorcommit="89a167d0d2efe98d983c87b5b84175b40ea55842" # version 1.0.0, March
 fetch_and_extract $dirname configure.sh https://github.com/arminbiere/lingeling/archive/${branchorcommit}.zip
 
 if grep -qE "exit \([01]\)" *.c *.h ; then
+    ./configure.sh
     for f in *.c *.h ; do
         sed -i 's/exit ([01])/abort()/g' $f
     done
 fi
 
 echo "[$dirname] Building ..."
-./configure.sh
 make
 echo "[$dirname] Build complete"
--- lib/yalsat/fetch-and-build.sh
+++ lib/yalsat/fetch-and-build.sh
@@ -8,15 +8,15 @@ fetch_and_extract $dirname configure.sh http://fmv.jku.at/yalsat/yalsat-03v.zip
 disable_fpu=false
 
 if grep -qE "exit \([01]\)" *.c *.h ; then
-for f in *.c *.h ; do
-    sed -i 's/exit ([01])/abort()/g' $f
-done
-fi
-if $disable_fpu && grep -qE '#ifdef __linux__' yals.c ; then
-    sed -i 's/#ifdef __linux__/#if 0/g' yals.c
+    ./configure.sh
+    for f in *.c *.h ; do
+        sed -i 's/exit ([01])/abort()/g' $f
+    done
+    if $disable_fpu && grep -qE '#ifdef __linux__' yals.c ; then
+        sed -i 's/#ifdef __linux__/#if 0/g' yals.c
+    fi
 fi
 
 echo "[$dirname] Building ..."
-./configure.sh
 make
 echo "[$dirname] Build complete"
