ef1d6dd34fabd2bce32454f598883442f1e9ac13
Ruben Götz <ruben.goetz@abgoetz.de>
2026-03-26 21:53:03 +0100

    moved palrup executables into mallob's build directory and fixed typo

--- lib/palrup/fetch-and-build.sh
+++ lib/palrup/fetch-and-build.sh
@@ -6,7 +6,7 @@ if [ ! -f CMakeLists.txt ]; then
     if [ ! -f palrup.zip ]; then
         echo "[palrup] Fetching sources ..."
         # for fixing a branch instead of a commit, prepend "refs/heads/"
-        branchorcommit="31f52fd70d39c1a6f7eeb3ce1bb7d97dbee6142d" # updated 2026-01-29
+        branchorcommit="5fc5c7577e723848934fbb03b59836f76be956f5" # updated 2026-01-29
         curl -L -o palrup.zip https://github.com/rubenGoetz/PalRUP-Check/archive/${branchorcommit}.zip
     fi
     echo "[palrup] Extracting sources ..."
@@ -25,3 +25,17 @@ cmake ..
 make
 cd ..
 echo "[palrup] Build complete"
+
+if ! [ -z "$1" ]; then
+    for x in local_check redistribute confirm; do
+        echo "[palrup] cp build/palrup_$x $1/palrup_$x"
+        cp build/palrup_$x "$1/palrup_$x"
+    done
+
+    for x in "" _launcher; do
+        echo "[palrup] cp build/pal${x}.sh $1/pal${x}.sh"
+        cp build/pal${x}.sh "$1/pal${x}.sh"
+    done
+
+    cp build/out.palrup_import.dummy "$1/out.palrup_import.dummy"
+fi
