66876d7cd10d6019c1ee9bd1399dffaf37dacec7
nrilu <11998876+nrilu@users.noreply.github.com>
2026-04-02 17:27:03 +0200

    fix: didn't allow minExitSwept=0, but needed for complete sweep

--- src/app/sweep/options.hpp
+++ src/app/sweep/options.hpp
@@ -14,6 +14,6 @@ OPT_INT(sweepMaxIterations, 	"swpmi", 	"sweep-max-iterations", 	3, 0, LARGE_INT,
 OPT_BOOL(sweepInitialCongruence,"swpic", 	"sweep-initial-congruence", true, "Let all solvers do one iteration of congruence before sweeping")
 OPT_INT(sweepMaxDepth, 			"swpmd", 	"sweep-max-depth", 			3, 1, LARGE_INT, "the maximum environment depth for sweeping")
 OPT_INT(sweepMaxEmptyRounds,    "swpmer",   "sweep-max-empty-rounds",   5, 1, LARGE_INT, "the maximum number of tolerated empty rounds (not a single unit or eqs found) before sweeping is terminated early")
-OPT_INT(sweepMinExitSwept,      "swpmes",   "sweep-min-exit-swept",     40000, 1, LARGE_INT, "minimum number of sweeps after which we start checking for early exit")
-OPT_INT(sweepTermNoProgress,    "swptnp",   "sweep-term-no-progress",   1, 0, 1, "terminate sweeping after an iteration made no progress at all (for both when it finished normally or exited early)")
+OPT_INT(sweepMinExitSwept,      "swpmes",   "sweep-min-exit-swept",     40000, 0, LARGE_INT, "minimum number of sweeps after which earlyexit gets checked. 0 = never exit early")
+OPT_INT(sweepTermNoProgress,    "swptnp",   "sweep-term-no-progress",   1, 0, 1, "terminate sweeping after an iteration made no progress at all")
 
