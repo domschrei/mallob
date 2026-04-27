c12202b7975e0c668335ca52cea2cd7b5dff6488
nrilu <11998876+nrilu@users.noreply.github.com>
2026-04-27 17:36:24 +0200

    add meson to spack environment

--- scripts/server/create_mallob_env.sh
+++ scripts/server/create_mallob_env.sh
@@ -4,10 +4,12 @@ echo "Usage: source scripts/server/create_mallob_env.sh [--fresh]"
 
 source /nfs/software/setup.sh  #Might be necessary to bootstrap spack itself if not already on the login node
 
+create=0
 
 if ! spack env list | grep -q mallob_env; then
     echo "Spack: creating mallob_env"
     spack env create mallob_env
+    create=1
 else
     echo "Spack: mallob_env already exists"
     if [ "$1" = "--fresh" ]; then
@@ -22,37 +24,45 @@ else
 	    return 0
 	else 
 	    spack env create mallob_env
+	    create=1
 	fi
     else
-	echo "Rerun with --fresh to force fresh install"
-	return 0
+	echo "Skipping env creation, directly activating it"
+	echo "For a fresh install, rerun with --fresh"
     fi
 fi
 
+
+if [ "$create" -eq 1 ]; then
+
+	spack env activate mallob_env
+	spack add cmake gcc jemalloc openmpi curl gdb meson
+	echo "Installing. Might take 1-2min."
+	spack concretize
+	spack install -j 32
+
+fi
+
+	#Verify
+	echo ""
+	echo GDB
+	echo $(gdb --version)
+	echo ""
+	echo JEMALLOC
+	echo $(jemalloc-config --version)
+	echo ""
+	echo CMAKE
+	echo $(cmake --version)
+	echo ""
+	echo MAKE
+	echo $(make --version)
+	echo ""
+	echo GCC
+	echo $(gcc --version)
+	echo "" 
+	echo MESON
+	echo $(meson --version)
+	echo "" 
+
 spack env activate mallob_env
-
-# spack config add "packages:elfutils:variants: ~debuginfod"   maybe not needed anymore with combined concretization
-spack add cmake gcc jemalloc openmpi curl gdb
-echo "Installing. Might take 1-2min."
-spack concretize
-spack install -j 32
-
-#Verify
-echo ""
-echo GDB
-echo $(gdb --version)
-echo ""
-echo JEMALLOC
-echo $(jemalloc-config --version)
-echo ""
-echo CMAKE
-echo $(cmake --version)
-echo ""
-echo MAKE
-echo $(make --version)
-echo ""
-echo GCC
-echo $(gcc --version)
-echo "" 
-
-spack env activate mallob_env
+echo "(script) Activated mallob_env"
