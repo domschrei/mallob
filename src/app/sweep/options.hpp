833d25e5f19167df5e40d7c8c072b505126ac07e
nrilu <11998876+nrilu@users.noreply.github.com>
2026-04-29 22:06:33 +0200

    prepare min depth for aggressive restarting

--- src/app/sweep/options.hpp
+++ src/app/sweep/options.hpp
@@ -12,7 +12,8 @@ OPT_INT(sweepSolverQuiet,		"swpqt", 	"sweep-solver-quiet", 		1, 0, 1, "whether t
 OPT_INT(sweepResweepChance, 	"swprc", 	"sweep-resweep-chance", 	1e4, 0, 1e4, "chance that a solver resweeps a variable from a found equivalence (in per mille)")
 OPT_INT(sweepMaxIterations, 	"swpmi", 	"sweep-max-iterations", 	3, 0, LARGE_INT, "max number of completed sweeps over all variables")
 OPT_BOOL(sweepInitialCongruence,"swpic", 	"sweep-initial-congruence", true, "Let all solvers do one iteration of congruence before sweeping")
-OPT_INT(sweepMaxDepth, 			"swpmd", 	"sweep-max-depth", 			3, 1, LARGE_INT, "the maximum environment depth for sweeping")
+OPT_INT(sweepMinDepth,			"swpmind",  "sweep-min-depth",			2, 2, LARGE_INT, "minimum environment depth for sweeping")
+OPT_INT(sweepMaxDepth, 			"swpmaxd", 	"sweep-max-depth", 			4, 2, LARGE_INT, "maximum environment depth for sweeping")
 OPT_INT(sweepMaxEmptyRounds,    "swpmer",   "sweep-max-empty-rounds",   5, 1, LARGE_INT, "the maximum number of tolerated empty rounds (not a single unit or eqs found) before sweeping is terminated early")
 OPT_INT(sweepMinExitSwept,      "swpmes",   "sweep-min-exit-swept",     40000, 0, LARGE_INT, "minimum number of sweeps after which earlyexit gets checked. 0 = never exit early")
 OPT_INT(sweepTermNoProgress,    "swptnp",   "sweep-term-no-progress",   1, 0, 1, "terminate sweeping after an iteration made no progress at all")
@@ -20,4 +21,3 @@ OPT_BOOL(sweepShuffleWork,		"swpsw", 	"sweep-shuffle-work",		true, "Shuffle the 
 OPT_BOOL(sweepToCompletion,		"swptoc", 	"sweep-to-completion",		false, "Sweep indefinitely to completion (or timeout). Increase env size only when no more progress")
 OPT_BOOL(sweepXJsendTo,			"swpxs", 	"sweep-xj-send",			true, "Sweep sends its units and equivalences via cjc to the concurrent SAT job ")
 OPT_BOOL(sweepXJrecvFrom,		"swpxr", 	"sweep-xj-recv",			true, "Sweep imports units via cjc from the concurrent SAT job")
-
