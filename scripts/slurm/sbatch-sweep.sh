6281dabad6d1aad13ebe59e4821bf6b7c8450781
nrilu <11998876+nrilu@users.noreply.github.com>
2026-05-04 21:23:44 +0200

    echo mkdir time...

--- scripts/slurm/sbatch-sweep.sh
+++ scripts/slurm/sbatch-sweep.sh
@@ -172,6 +172,8 @@ for i in $(seq $DS_FIRSTJOBIDX $DS_LASTJOBIDX | shuf) ; do
   # -sweep-growing-environments=1 \
 
     # Pre-create network-disk output directories to avoid many concurrent filesystem manips
+    #
+    echo "$(date) mkdir of output logdirs"
     mkdir -p $(for rank in $(seq 0 $(($SLURM_NTASKS-1))); do echo $outputlogdir/$i/$rank; done)
 
     export MALLOB_GLOBALLOGDIR=$globallogdir
