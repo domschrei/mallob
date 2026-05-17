85d36cf817091ea8ea4e2e4a04ee5a4bf2ec1b4d
Dominik Schreiber <mail@dominikschreiber.de>
2026-05-17 21:09:03 +0200

    Switch to Satsuma with Cliquer

--- lib/extsatsuma/fetch-and-build.sh
+++ lib/extsatsuma/fetch-and-build.sh
@@ -3,8 +3,8 @@
 source ../base-build-functions.sh
 dirname="satsuma"
 
-branchorcommit="16d6b0c889f849ef6af17849c0df933ee49b1aef"
-fetch_and_extract $dirname CMakeLists.txt https://github.com/domschrei/satsuma/archive/${branchorcommit}.zip
+branchorcommit="4cfdbe000613935244b5b60e40db627fb682c0cb"
+fetch_and_extract $dirname CMakeLists.txt https://github.com/domschrei/satsuma-with-cliquer/archive/${branchorcommit}.zip
 
 echo "[$dirname] Building ..."
 mkdir -p build
