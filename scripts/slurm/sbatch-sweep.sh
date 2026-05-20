69d76318f08b91c40509cde7c9a9a3ef988642e6
nrilu <11998876+nrilu@users.noreply.github.com>
2026-05-20 16:48:06 +0200

    fix backslash

--- scripts/slurm/sbatch-sweep.sh
+++ scripts/slurm/sbatch-sweep.sh
@@ -158,7 +158,7 @@ for i in $(seq $DS_FIRSTJOBIDX $DS_LASTJOBIDX | shuf) ; do
 	-sweep-sharing-period=0.050 \
   -sweep-success-ratio=0.01 \
   -sweep-success-window=100 \
-  -sweep-success-skips=3 \
+  -sweep-success-skips=3
 "
 
     # Pre-create network-disk output directories to avoid many concurrent filesystem manips
