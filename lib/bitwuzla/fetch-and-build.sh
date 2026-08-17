778052cdaa07bd3943c0a0f15f5b75de45aad43c
Dominik Schreiber <mail@dominikschreiber.de>
2026-08-17 17:27:13 +0200

    Update Bitwuzla

--- lib/bitwuzla/fetch-and-build.sh
+++ lib/bitwuzla/fetch-and-build.sh
@@ -3,7 +3,7 @@
 source ../base-build-functions.sh
 dirname="bitwuzla"
 
-branchorcommit="af2f6029d4cf978decb7652b4902210998001e1a" # main branch, updated 2026-05-28
+branchorcommit="5027ca9a598cb6d5cf392767c80c77e0478d86bd" # main branch, updated 2026-08-17
 fetch_and_extract $dirname configure.py https://github.com/bitwuzla/bitwuzla/archive/${branchorcommit}.zip
 
 echo "[$dirname] Building ..."
