19755bb6c921e89924455a650319a4eb7b48a789
Dominik Schreiber <dev@dominikschreiber.de>
2026-08-20 15:13:29 +0200

    Update fetch-and-build.sh
    Corrected modification date

--- lib/palrup/fetch-and-build.sh
+++ lib/palrup/fetch-and-build.sh
@@ -3,7 +3,7 @@
 source ../base-build-functions.sh
 dirname="PalRUP-Check"
 
-branchorcommit="9357fd160ac53e81cc701e6d2616617bd774d6c0" # updated 2026-01-29
+branchorcommit="9357fd160ac53e81cc701e6d2616617bd774d6c0" # updated 2026-08-20
 fetch_and_extract $dirname CMakeLists.txt https://github.com/rubenGoetz/PalRUP-Check/archive/${branchorcommit}.zip
 
 sed -i 's/-Werror//g' CMakeLists.txt
