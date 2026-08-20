8fa740298b33f0d0a4c01b3829c559a47ee3e0fe
Ruben Götz <ruben.goetz@abgoetz.de>
2026-08-20 10:53:23 +0200

    added palrup presets

--- lib/palrup/fetch-and-build.sh
+++ lib/palrup/fetch-and-build.sh
@@ -3,7 +3,7 @@
 source ../base-build-functions.sh
 dirname="PalRUP-Check"
 
-branchorcommit="31f52fd70d39c1a6f7eeb3ce1bb7d97dbee6142d" # updated 2026-01-29
+branchorcommit="9357fd160ac53e81cc701e6d2616617bd774d6c0" # updated 2026-01-29
 fetch_and_extract $dirname CMakeLists.txt https://github.com/rubenGoetz/PalRUP-Check/archive/${branchorcommit}.zip
 
 sed -i 's/-Werror//g' CMakeLists.txt
