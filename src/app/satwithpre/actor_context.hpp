209d211a5d11200c7899f9385e9994942432d10b
Dominik Schreiber <mail@dominikschreiber.de>
2026-09-02 17:56:21 +0200

    Add missing file

---
+++ src/app/satwithpre/actor_context.hpp
@@ -0,0 +1,21 @@
+
+#pragma once
+
+#include "app/satwithpre/sat_preprocess_actor.hpp"
+#include <memory>
+#include <vector>
+
+struct ActorContext {
+    std::string id;
+    enum ActorType {SATSUMA_INT, SATSUMA_EXT, KISSAT, LINGELING, MALLOBSAT} type;
+    ActorContext* prerequisite {nullptr};
+    std::vector<ActorContext*> actorsBeingDisplaced;
+    bool onlyStartIfPrerequisiteSimplified {false};
+
+    std::unique_ptr<SatPreprocessActor> actor;
+    enum ActiveActorState {UNINITIALIZED, RUNNING, FINISHED} state {UNINITIALIZED};
+    SatPreprocessActor::PreprocessActorResult result {SatPreprocessActor::PENDING};
+    std::vector<int> formula;
+    std::vector<int> model;
+    float timeOfSignalledDisplacement {0};
+};
