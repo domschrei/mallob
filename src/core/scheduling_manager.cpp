2ebb1af2427e38929c6c19390a0272a0f6ad4c60
Dominik Schreiber <mail@dominikschreiber.de>
2026-09-03 16:52:53 +0200

    Avoid double deletion of jobs tied to uncommitting

--- src/core/scheduling_manager.cpp
+++ src/core/scheduling_manager.cpp
@@ -1114,7 +1114,6 @@ JobRequest SchedulingManager::uncommit(Job& job, bool leaving) {
         LOG(V4_VVER, "destruct request(s) to multiply\n");
         job.getRequestToMultiply(/*left=*/true).reset();
         job.getRequestToMultiply(/*left=*/false).reset();
-        if (!job.hasDescription()) eraseJobAndQueueForDeletion(job);
     }
     return optReq.value();
 }
