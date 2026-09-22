b6c6ae5b21e403228f0a9683c80dbdac7d2384cd
nrilu <11998876+nrilu@users.noreply.github.com>
2026-09-22 12:36:56 +0200

    sbatch ajdust

--- scripts/slurm/sbatch.sh
+++ scripts/slurm/sbatch.sh
@@ -120,10 +120,11 @@ while [ $(( $(date +%s) - $starttime + $DS_SECONDSPERJOB + 30 )) -lt $DS_RUNTIME
     timeout=$DS_SECONDSPERJOB
     cmd="$build/mallob \
 	-mono-app=$DS_APP \
-	-pb=1 -pjp=999999 -pef=1 -mono=$f -jwl=$timeout -T=$(($timeout+30)) -wam=60``000 -pre-cleanup=1 \
-    -q=1 -log=$globallogdir -tmp=$localtmpdir -comment-outputlogdir=$outputlogdir -sro=${globallogdir}/processed-jobs.out -trace-dir=${globallogdir}/ -os=1 -v=3 -iff=0 -s2f=${globallogdir}/model -cm=0 \
-    -rpa=1 -pph=${SLURM_NTASKS_PER_NODE} -mlpt=50``000``000 -t=$((${SLURM_CPUS_PER_TASK} / 2)) \
     -satsolver=k \
+	-pb=1 -pjp=999999 -pef=1 -mono=$f -jwl=$timeout -T=$(($timeout+30)) -wam=60``000 -pre-cleanup=1 \
+    -q=1 -log=$globallogdir -tmp=$localtmpdir -comment-outputlogdir=$outputlogdir -sro=${globallogdir}/processed-jobs.out \
+	-trace-dir=${globallogdir}/ -os=1 -iff=0 -s2f=${globallogdir}/model -cm=0 \
+    -rpa=1 -pph=${SLURM_NTASKS_PER_NODE} -mlpt=50``000``000 -t=$((${SLURM_CPUS_PER_TASK} / 2)) \
     -scll=60 -slbdl=60 -qcll=60 -qlbdl=60 -csm=3 -cfm=3 -cfci=30 -mscf=5 -bem=1 -aim=1 \
     -seed=0 \
   -jcup=0.05 \
@@ -163,7 +164,7 @@ while [ $(( $(date +%s) - $starttime + $DS_SECONDSPERJOB + 30 )) -lt $DS_RUNTIME
     $mpicall bash -c "scripts/slurm/prolog.sh ; $cmd"
 
     sleep 3 # avoid "nodes are still busy" issue?
-    $mpicall scripts/slurm/epilogNew.sh
+    $mpicall "scripts/slurm/epilogNew.sh"
 
     echo "$(date) JOB $i FINISHED"
 
